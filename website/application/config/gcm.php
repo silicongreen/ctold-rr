a609186d530ae580aaa4b5c87e68c2e348c0f508
fahim <fahim.mohammad@teamworkbd.com>
2014-11-26 13:53:41 +0600

    notification start

--- website/application/config/gcm.php
+++ website/application/config/gcm.php
@@ -12,7 +12,7 @@
 |--------------------------------------------------------------------------
 | Get API Key: https://code.google.com/apis/console/
 */
-$config['gcm_api_key'] = 'AIzaSyDe9D5sao_eI6TpBcZInTVEw6UCBTu_H1M';
+$config['gcm_api_key'] = 'AIzaSyAdart24S8P6VzhOHouR_1H_-MSBd5aRek';
 
 
 /*

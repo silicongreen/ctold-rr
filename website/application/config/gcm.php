35435a18c0b8142c74f5f15adb9d32e7f989cc86
fahim <fahim.mohammad@teamworkbd.com>
2014-12-01 13:09:08 +0600

    api key test

--- website/application/config/gcm.php
+++ website/application/config/gcm.php
@@ -13,7 +13,7 @@
 | Get API Key: https://code.google.com/apis/console/
 */
 //$config['gcm_api_key'] = 'AIzaSyBrKEjz2fYKuBiNJwtKD09DtmRZKkEeFYk';
-$config['gcm_api_key'] = 'AIzaSyDWHGP2XTXQCkMuegLzMjHYNcep78lJ73E';
+$config['gcm_api_key'] = 'AIzaSyBrKEjz2fYKuBiNJwtKD09DtmRZKkEeFYk';
 
 
 

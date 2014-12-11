242b77ccebe864a7b544df3edae22ce788783532
fahim <fahim.mohammad@teamworkbd.com>
2014-12-11 12:47:29 +0600

    wow

---
+++ src/api/protected/modules/api/models/Wow.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * This is the model class for table "tds_tags".
+ *
+ * The followings are the available columns in table 'tds_tags':
+ * @property integer $id
+ * @property string $tags_name
+ * @property string $hit_count
+ *
+ * The followings are the available model relations:
+ * @property PostTags[] $postTags
+ */
+class Wow extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'tds_user_post_wow';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('user_id,post_id', 'required'),
+			array('id, user_id, post_id', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		
+		return array();
+	}
+
+	
+	
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return Tag the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+        public function wowexists($post_id,$user_id)
+        {
+            $criteria = new CDbCriteria;
+            $criteria->select = 't.id';
+            $criteria->compare('post_id', $post_id);
+            $criteria->compare('user_id', $user_id);
+            $criteria->limit = 1;
+            $obj_wow = $this->find($criteria);
+            
+            if($obj_wow)
+            {
+               return true; 
+            }
+            
+            return false;
+            
+        }
+        
+        public function userwow($user_id)
+        {
+            $criteria = new CDbCriteria;
+            $criteria->select = 't.post_id';
+            $criteria->compare('user_id', $user_id);
+            $obj_wow = $this->findAll($criteria);
+            $obj_wow_array = array();
+            
+            if($obj_wow)
+            {
+               foreach($obj_wow as $value)
+               {
+                  $obj_wow_array[] =  $value->post_id;
+               }    
+            }
+            
+            return $obj_wow_array;
+            
+        }
+        
+        
+        
+}

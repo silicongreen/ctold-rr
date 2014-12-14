df1f228c71783c4887aac6cd74806205e83ef6aa
fahim <fahim.mohammad@teamworkbd.com>
2014-12-14 16:58:04 +0600

    read later space

--- website/application/widgets/actionbox/view.php
+++ website/application/widgets/actionbox/view.php
@@ -26,6 +26,6 @@ $wow_class = getclassactionbox($news->wow_count);
         </div>
     </div>
     <div class="col-lg-4" >
-        <div style="margin:9px auto !important; float:none !important; display:block !important;"  data="read_later" id="read_later_<?php echo $news->post_id; ?>" class="f2 read_later <?php echo ( free_user_logged_in() ) ? "" : "before-login-user"; ?>">&nbsp;&nbsp;Read Later</div>
+        <div style="margin:9px auto !important; float:none !important; display:block !important;"  data="read_later" id="read_later_<?php echo $news->post_id; ?>" class="f2 read_later <?php echo ( free_user_logged_in() ) ? "" : "before-login-user"; ?>">&nbsp;&nbsp;&nbsp;Read Later</div>
     </div>
  </div>   

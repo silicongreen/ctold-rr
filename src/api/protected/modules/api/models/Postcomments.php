3b8553b9f74d5d88de0d83cffc7bff414447cd80
fahim <fahim.mohammad@teamworkbd.com>
2014-12-15 13:19:12 +0600

    commit in master

---
+++ src/api/protected/modules/api/models/Postcomments.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * This is the model class for table "tds_tags".
+ *
+ * The followings are the available columns in table 'tds_tags':
+ * @property integer $id
+ * @property string $tags_name
+ * @property string $hit_count
+ *
+ * The followings are the available model relations:
+ * @property PostTags[] $postTags
+ */
+class Postcomments extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'tds_post_comments';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('user_id,post_id,title,details', 'required'),
+			array('user_id,post_id,title,details,created_date,show_comment', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array('post' => array(self::BELONGS_TO, 'Post', 'post_id'),);
+	}
+
+	
+	
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return Tag the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+        public function getCommentsPost($post_id,$user_post=false)
+        {
+            $criteria = new CDbCriteria;
+            $criteria->select = 't.id,t.title,t.details,t.created_date';
+            $criteria->order = "t.created_date DESC";
+            $criteria->compare('post_id', $post_id);
+            if($user_post==false)
+            {
+                $criteria->compare('show_comment', 1);
+            }  
+            $criteria->with = array(
+                        'post' => array(
+                            'select' => '',
+                            'joinType' => "INNER JOIN"
+                        )
+            );
+            $criteria->limit = 30;
+            $obj_comments_post = $this->findAll($criteria);
+            $comments_post = array();
+            
+            if($obj_comments_post)
+            foreach($comments_post as $value)
+            {
+                $comments_post[$i]['id'] = $value->id;
+                $comments_post[$i]['title'] = $value->title;
+                $comments_post[$i]['details'] = $value->details;
+                $comments_post[$i]['created_date'] = $value->created_date;
+            }    
+            return $comments_post;
+            
+        }
+        
+        
+        
+}

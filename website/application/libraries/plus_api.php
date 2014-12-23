8873319bdc3fc853ba61357a6354e00db77c4903
fahim <fahim.mohammad@teamworkbd.com>
2014-12-23 16:14:16 +0600

    plus api

--- website/application/libraries/plus_api.php
+++ website/application/libraries/plus_api.php
@@ -17,7 +17,47 @@ class Plus_api {
     private $_oauth_endpoint;
 
    
-   
+   public function init($ar_params, $b_use_session = true) {
+
+        $this->_CI = & get_instance();
+
+        $this->_CI->load->config('plus');
+        $endPoint = $this->_CI->config->config['plus_api_endpoint'];
+
+        $school_code = ( isset($ar_params['school_code']) && !empty($ar_params['school_code'])) ? $ar_params['school_code'] : '';
+        $username = ( isset($ar_params['username']) && !empty($ar_params['username'])) ? $school_code . '-' . $ar_params['username'] : $school_code . '-champs21';
+        $password = ( isset($ar_params['password']) && !empty($ar_params['password'])) ? $ar_params['password'] : $school_code . $this->_CI->config->config['champs_password'];
+
+        $this->_username = $username;
+        $this->_password = $password;
+
+        $this->_client_id = sha1($school_code . $this->_CI->config->config['client_id']);
+        $this->_client_secret = sha1($school_code . $this->_CI->config->config['client_secret']);
+        $this->_redirect_url = 'http://' . $school_code . '.' . $endPoint . '/authenticate';
+
+        $this->_api_endpoint = "http://" . $school_code . '.' . $endPoint . "/api/";
+
+        $this->_oauth_endpoint = "http://" . $school_code . '.' . $endPoint . "/";
+
+        $this->_client = new Client($this->_api_endpoint);
+        $this->_client_oauth = new Client($this->_oauth_endpoint);
+
+
+        if (isset($_SESSION['plus_access_token']) && $b_use_session) {
+
+            if (isset($this->_token) && ($this->_token != $_SESSION['plus_access_token'])) {
+
+                $this->_token = $_SESSION['plus_access_token'];
+            } else if (!isset($this->_token)) {
+
+                $this->_token = $_SESSION['plus_access_token'];
+            }
+
+            return $_SESSION['plus_access_token'];
+        }
+
+        return $this->getAccessToken();
+    }
 
 }
 

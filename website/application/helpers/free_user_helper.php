0349e446b0feff250cd9938700aeca33bede0139
fahim <fahim.mohammad@teamworkbd.com>
2014-12-23 15:08:45 +0600

    test code 2

--- website/application/helpers/free_user_helper.php
+++ website/application/helpers/free_user_helper.php
@@ -131,14 +131,14 @@ if (!function_exists('set_user_sessions')) {
                     'school_code' => $obj_user->paid_school_code
                 );
 
-                $int_response = $CI->plus_api->init($ar_params, true);
-
-                if($int_response != FALSE)
-                {
-                    echo "here";
-                    $res = $CI->plus_api->call__('get', 'users/loginhook', 'get_data_login');
-                    var_dump($res);
-                }
+//                $int_response = $CI->plus_api->init($ar_params, true);
+//
+//                if($int_response != FALSE)
+//                {
+//                    echo "here";
+//                    $res = $CI->plus_api->call__('get', 'users/loginhook', 'get_data_login');
+//                    var_dump($res);
+//                }
         }
         
         if($remeber)

953491ef22d09cb08ba32abb0d258f92e9438cd7
fahim <fahim.mohammad@teamworkbd.com>
2014-12-23 15:12:23 +0600

    back to normal

--- website/application/helpers/free_user_helper.php
+++ website/application/helpers/free_user_helper.php
@@ -123,13 +123,13 @@ if (!function_exists('set_user_sessions')) {
         $sessionData['free_user']['paid_school_id'] = $obj_user->paid_school_id;
         $sessionData['free_user']['profile_image'] = $obj_user->profile_image;
         
-        if($obj_user->paid_school_code && $obj_user->paid_username && $obj_user->paid_password)
-        {
-                $CI->load->library('plus_api');
+        //if($obj_user->paid_school_code && $obj_user->paid_username && $obj_user->paid_password)
+        //{
+               // $CI->load->library('plus_api');
 
-                $ar_params = array(
-                    'school_code' => $obj_user->paid_school_code
-                );
+//                $ar_params = array(
+//                    'school_code' => $obj_user->paid_school_code
+//                );
 
 //                $int_response = $CI->plus_api->init($ar_params, true);
 //
@@ -139,7 +139,7 @@ if (!function_exists('set_user_sessions')) {
 //                    $res = $CI->plus_api->call__('get', 'users/loginhook', 'get_data_login');
 //                    var_dump($res);
 //                }
-        }
+        //}
         
         if($remeber)
         {

de562c0d02f113db68c7b56e3c2564c4c0e10c12
fahim <fahim.mohammad@teamworkbd.com>
2014-12-28 16:34:27 +0600

    roll number added

--- src/api/protected/modules/api/controllers/CalenderController.php
+++ src/api/protected/modules/api/controllers/CalenderController.php
@@ -354,27 +354,27 @@ class CalenderController extends Controller {
             {
                 if($value['status']==1)
                 {
-                   $student['present'][$present]['name'] = $value['student_name'];
-                   $student['present'][$present]['roll'] = $value['roll_no'];
+                   $student['present'][$present]['student_name'] = $value['student_name'];
+                   $student['present'][$present]['roll_no'] = $value['roll_no'];
                    $present++; 
                   
                 }    
                 else if($value['status']==0)
                 {
-                   $student['absent'][$absent]['name'] = $value['student_name'];
-                   $student['absent'][$absent]['roll'] = $value['roll_no'];
+                   $student['absent'][$absent]['student_name'] = $value['student_name'];
+                   $student['absent'][$absent]['roll_no'] = $value['roll_no'];
                    $absent++;
                 } 
                 else if($value['status']==2)
                 {
-                   $student['late'][$late]['name'] = $value['student_name'];
-                   $student['late'][$late]['roll'] = $value['roll_no'];
+                   $student['late'][$late]['student_name'] = $value['student_name'];
+                   $student['late'][$late]['roll_no'] = $value['roll_no'];
                    $late++; 
                 } 
                 else if($value['status']==0)
                 {
-                   $student['leave'][$leave]['name'] = $value['student_name'];
-                   $student['leave'][$leave]['roll'] = $value['roll_no'];
+                   $student['leave'][$leave]['student_name'] = $value['student_name'];
+                   $student['leave'][$leave]['roll_no'] = $value['roll_no'];
                    $leave++;
                 } 
                     

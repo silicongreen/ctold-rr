59c37f64de492462fab2c5fdb8e3f466256d98a2
admin <likhon_13@yahoo.com>
2015-01-03 13:02:31 +0600

    free school feed for paid user

--- website/application/modules/front/views/school_feed_for_paid.php
+++ website/application/modules/front/views/school_feed_for_paid.php
@@ -4,3 +4,33 @@ $widget = new Widget;
 $widget->run('postdata', "index", $s_category_ids, 'index');
 ?>
 <script type="text/javascript" src="<?php echo base_url('js/main-bottom.js'); ?>"></script>
+<script type="text/javascript">
+
+$(window).load(function ()
+{
+    UpdateDimensions();
+});
+ 
+function UpdateDimensions()
+{
+try
+{
+var height = $("#main").height() + 130;
+var width = $("body").outerWidth() + 100;
+var ReferalUrl = "" 
+if (window.postMessage)
+    parent.postMessage(height + "," + width, '*');
+}
+catch (c)
+{
+alert(c.message);
+}
+}
+</script>
+
+<style>
+    body
+    {
+        background:#e7e7e7 !important;
+    }
+</style>

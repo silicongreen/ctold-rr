a2cfe9c5aaf5c408a6f1a56dcc52799289b7e724
fahim <fahim.mohammad@teamworkbd.com>
2015-01-05 17:22:55 +0600

    fees collection

--- src/api/protected/modules/api/controllers/EventController.php
+++ src/api/protected/modules/api/controllers/EventController.php
@@ -22,13 +22,36 @@ class EventController extends Controller {
             array('allow', // allow authenticated user to perform 'create' and 'update' actions
                 'actions' => array('index', 'acknowledge','meetingrequest','meetingstatus',
                     'getstudentparent','addmeetingrequest','addmeetingparent','getteacherparent',
-                    'addleaveteacher','leavetype','teacherleaves','studentleaves'),
+                    'addleaveteacher','leavetype','teacherleaves','studentleaves','fees'),
                 'users' => array('@'),
             ),
             array('deny', // deny all users
                 'users' => array('*'),
             ),
         );
+    }
+    
+    public function actionFees()
+    {
+        $user_secret = Yii::app()->request->getPost('user_secret');
+        
+        $student_id = Yii::app()->request->getPost('student_id');
+        if(Yii::app()->user->user_secret === $user_secret && Yii::app()->user->isParent  && $student_id )
+        {
+            $objFees = new FinanceFees();
+            
+            $response['data']['due'] = $objFees->feesStudentDue($student_id);
+            $response['data']['history'] = $objFees->feesStudentDueHistory($student_id);
+            $response['status']['code'] = 200;
+            $response['status']['msg'] = "Success";
+        } 
+        else
+        {
+            $response['status']['code'] = 400;
+            $response['status']['msg'] = "Bad Request";
+        }
+        echo CJSON::encode($response);
+        Yii::app()->end();
     }
     
     public function actionStudentLeaves()

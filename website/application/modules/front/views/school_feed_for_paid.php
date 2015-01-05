c2a856e6aff4db579c8d78a7622bf0efb044aa04
admin <likhon_13@yahoo.com>
2015-01-05 18:00:14 +0600

    paid reminder design and free school feed for paid

--- website/application/modules/front/views/school_feed_for_paid.php
+++ website/application/modules/front/views/school_feed_for_paid.php
@@ -1,7 +1,7 @@
 <script type="text/javascript" src="<?php echo base_url('js/top-main.js'); ?>"></script> 
 <?php
 $widget = new Widget;
-$widget->run('postdata', "index", $s_category_ids, 'index');
+$widget->run('postdataforpaid', "index", $s_category_ids, 'index');
 ?>
 <script type="text/javascript" src="<?php echo base_url('js/main-bottom.js'); ?>"></script>
 <script type="text/javascript">

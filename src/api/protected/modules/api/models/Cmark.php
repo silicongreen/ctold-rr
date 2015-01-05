75a5564687d76799d5f71998c223f13d37179e6f
fahim <fahim.mohammad@teamworkbd.com>
2015-01-05 18:35:19 +0600

    sending assessment id

--- src/api/protected/modules/api/models/Cmark.php
+++ src/api/protected/modules/api/models/Cmark.php
@@ -56,7 +56,7 @@ class Cmark extends CActiveRecord
             $i = 0;
             foreach ($data as $value)
             {
-                $response_array[$i]['id'] = $value->id;
+                $response_array[$i]['id'] = $value['assessment']->id;
                 $response_array[$i]['mark'] = $value->mark;
                 $response_array[$i]['created_date'] = $value->created_date;
                 $response_array[$i]['title'] = $value['assessment']->title;

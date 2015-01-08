60a9c05bdd2bdf802c0c68ec8c02f8805c5206a7
fahim <fahim.mohammad@teamworkbd.com>
2015-01-08 14:31:49 +0600

    banner image change

--- website/application/modules/front/views/post.php
+++ website/application/modules/front/views/post.php
@@ -663,7 +663,7 @@
            var ar_src = sourcesrc.split("/");
            var imagename = ar_src[ar_src.length-1];
            var ar_image = imagename.split(".");
-           var hover_image = $("#base_url").val()+"styles/layouts/tdsfront/images/ads/"+ar_image[0]+"_hover."+ar_image[1];
+           var hover_image = $("#base_url").val()+"styles/layouts/tdsfront/images/ads/final/"+ar_image[0]+"_hover."+ar_image[1];
            $(this).attr("src",hover_image);
        });
        $(document).on("mouseout",".ads-image",function(){

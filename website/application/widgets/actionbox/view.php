88fd98bc1f8d705cde0a4fa50b88edcaf920ffe4
fahim <fahim.mohammad@teamworkbd.com>
2015-01-18 14:46:10 +0600

    share icon

--- website/application/widgets/actionbox/view.php
+++ website/application/widgets/actionbox/view.php
@@ -25,17 +25,24 @@ $wow_class = getclassactionbox($news->wow_count);
                 <div class="seen col-lg-<?php echo $seen_class['class2']; ?>" style="margin-top:8px;" ><span  class="f2"  style="font-size:12px;margin-left:5px;margin-top:6px;color:#666">Seen <?php echo $seen_class['new_count']; ?></span></div>
             </div>
     </div>
+  
     <div class="col-lg-3" style="border-right:1px solid #DADEDF; min-height: 39px;">
         <a class="addthis_button_compact" id="addthis_<?php echo $news->post_id; ?>" <?php if($news->can_share==1): ?> style="display:none;" <?php endif; ?> addthis:url="<?php echo create_link_url(NULL, $news->headline,$news->post_id,false,true,false); ?>" addthis:title="<?php echo $news->headline; ?>" id="full_leader_board">
-            <span class="clearfix f2" style="float:left;font-size:12px;margin-left:29px;margin-top:6px;color:#666">
-                Share
-            </span>
+            <div class="seen-image col-lg-2" style="text-align:right;margin-top:12px;margin-left:10px;" ><img src="<?php echo base_url("styles/layouts/tdsfront/images/social/ch.gif"); ?>" /></div>
+            <div class="seen col-lg-8" >
+                <span class="clearfix f2" style="float:left;font-size:12px;margin-left:8px;margin-top:8px;color:#666">
+                    Share
+                </span>
+            </div>    
         </a> 
         <?php if($news->can_share==1): ?> 
             <a href="javascript:void(0);" onclick="sharebrowser(<?php echo $news->post_id;?>)">
-                <span class="clearfix f2" style="float:left;font-size:12px;margin-left:29px;margin-top:6px;color:#666">
-                    Share
-                </span>
+                <div class="seen-image col-lg-2" style="text-align:right;margin-top:12px;margin-left:10px;" ><img src="<?php echo base_url("styles/layouts/tdsfront/images/social/ch.gif"); ?>" /></div>
+                    <div class="seen col-lg-8" >
+                        <span class="clearfix f2" style="float:left;font-size:12px;margin-left:8px;margin-top:8px;color:#666">
+                            Share
+                        </span>
+                    </div>    
             </a>
         <?php endif; ?>
     </div>

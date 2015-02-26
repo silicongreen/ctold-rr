dfb44f9bd2882fdfac09beba1e7891e1a7922853
fahim <fahim.mohammad@teamworkbd.com>
2015-02-26 14:43:57 +0600

    make post_type string

--- src/api/protected/modules/api/controllers/DashboardController.php
+++ src/api/protected/modules/api/controllers/DashboardController.php
@@ -99,7 +99,7 @@ class DashboardController extends Controller
 
                 $school_obj = new Schools();
                 $school_details = $school_obj->findByPk($school_id);
-                $response['maindata']['post_type'] = 20;
+                $response['maindata']['post_type'] = "20";
                 $response['maindata']['school_name'] = $school_details->name;
                 $response['maindata']['school_picture'] = "";
 

aecbc707d6c7f83ae7355730bf3ed598e9842e85
fahim <fahim.mohammad@teamworkbd.com>
2015-03-03 13:36:06 +0600

    single notice acknowladge

--- src/api/protected/modules/api/controllers/CalenderController.php
+++ src/api/protected/modules/api/controllers/CalenderController.php
@@ -170,6 +170,7 @@ class CalenderController extends Controller
                         $leave_array_modified[] = $merge;
                         $leave_count++;
                     }
+                    $week_day_id = $end_holiday->format("w");
                     if (!in_array($end_holiday->format("Y-m-d"), $holiday_array_for_count) && !in_array($end_holiday->format("w"), $weekend_array))
                     {
                         $merge['title'] = $value['title'];
@@ -413,6 +414,9 @@ class CalenderController extends Controller
                 else
                 {
                     $response['data'] = $attendance_array;
+                    $response['data']['week_day_id'] = $week_day_id;
+                    $response['data']['week_day_array'] = $weekend_array;
+                    
                     $response['data']['holiday'] = $holiday_array;
                     $response['data']['leave'] = $leave_array_modified;
                     $response['data']['total'] = $i;

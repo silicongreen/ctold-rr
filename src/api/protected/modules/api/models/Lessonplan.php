5d81812e3917f3c6e50d290fb72a4771079b7539
fahim <fahim.mohammad@teamworkbd.com>
2015-03-24 16:57:35 +0600

    lesson plan category model name uppercase later

--- src/api/protected/modules/api/models/Lessonplan.php
+++ src/api/protected/modules/api/models/Lessonplan.php
@@ -41,7 +41,7 @@ class Lessonplan extends CActiveRecord
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
-                        'category' => array(self::BELONGS_TO, 'lessonplancategory', 'lessonplan_category_id')
+                        'category' => array(self::BELONGS_TO, 'LessonplanCategory', 'lessonplan_category_id')
                 );
 	}
 

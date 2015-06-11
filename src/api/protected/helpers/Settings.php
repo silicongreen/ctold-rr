42edd8f348064b138c07650cf0d99868bb5614fd
fahim <fahim.mohammad@teamworkbd.com>
2015-06-11 16:38:38 +0600

    bing token and google problem

--- src/api/protected/helpers/Settings.php
+++ src/api/protected/helpers/Settings.php
@@ -148,7 +148,51 @@ class Settings
         )
     );
     
-    
+    public static function getBingTokens($grantType, $scopeUrl, $clientID, $clientSecret, $authUrl) {
+        try {
+            //Initialize the Curl Session.
+            $ch = curl_init();
+            //Create the request Array.
+            $paramArr = array(
+                'grant_type' => $grantType,
+                'scope' => $scopeUrl,
+                'client_id' => $clientID,
+                'client_secret' => $clientSecret
+            );
+
+            //Create an Http Query.//
+            $paramArr = http_build_query($paramArr);
+            //Set the Curl URL.
+            curl_setopt($ch, CURLOPT_URL, $authUrl);
+            //Set HTTP POST Request.
+            curl_setopt($ch, CURLOPT_POST, TRUE);
+            //Set data to POST in HTTP "POST" Operation.
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $paramArr);
+            //CURLOPT_RETURNTRANSFER- TRUE to return the transfer as a string of the return value of curl_exec().
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+            //CURLOPT_SSL_VERIFYPEER- Set FALSE to stop cURL from verifying the peer's certificate.
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            //Execute the  cURL session.
+            $strResponse = curl_exec($ch);
+            
+            //Get the Error Code returned by Curl.
+            $curlErrno = curl_errno($ch);
+            if ($curlErrno) {
+                $curlError = curl_error($ch);
+                throw new Exception($curlError);
+            }
+            //Close the Curl Session.
+            curl_close($ch);
+            //Decode the returned JSON string.
+            $objResponse = json_decode($strResponse);
+            if (isset( $objResponse->error )) {
+                throw new Exception($objResponse->error_description);
+            }
+            return $objResponse->access_token;
+        } catch (Exception $e) {
+            echo "Exception-" . $e->getMessage();
+        }
+    }
     public static function download_bing_audio($strWord)
     {
         $sound_status = 0;
@@ -157,7 +201,7 @@ class Settings
         $authUrl = 'https://datamarket.accesscontrol.windows.net/v2/OAuth2-13/';
         $scopeUrl = 'http://api.microsofttranslator.com';
         $grantType = 'client_credentials';
-        $accessToken = getBingTokens( $grantType, $scopeUrl, $clientID, $clientSecret, $authUrl );
+        $accessToken = self::getBingTokens( $grantType, $scopeUrl, $clientID, $clientSecret, $authUrl );
         if($accessToken)
         {
             try
@@ -201,7 +245,7 @@ class Settings
 
             curl_exec( $objCURL );
             $curl_status = curl_getinfo ($objCURL);
-            if($curl_status['http_code']==200 && filesize($strMusicFile)>700)
+            if($curl_status['http_code']==200)
             {
                 $sound_status = 1;
                 curl_close( $objCURL );

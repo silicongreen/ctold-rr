8f42d83143ea2e332685cb5a77edae185ef3e022
Nurul Islam <webdev.nislam@gmail.com>
2015-06-14 13:04:31 +0600

    DEBUG: admin panel auto logout progress

--- website/application/modules/admin/controllers/news.php
+++ website/application/modules/admin/controllers/news.php
@@ -29,8 +29,6 @@ class news extends MX_Controller
      */
     public function index($school_id=0)
     {
-
-          
         //set table id in table open tag
         $tmpl = array('table_open' => '<table id="big_table" border="1" cellpadding="2" cellspacing="1" class="mytable">');
         $this->table->set_template($tmpl);

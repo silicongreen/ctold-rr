6e83683e78be64aa2002c6f7f1d28b56a9c4de7b
fahim <fahim.mohammad@teamworkbd.com>
2015-06-24 12:48:06 +0600

    what the hell

--- src/api/protected/modules/api/controllers/FreeuserController.php
+++ src/api/protected/modules/api/controllers/FreeuserController.php
@@ -3662,7 +3662,7 @@ class FreeuserController extends Controller
         
         
         $responsesss = array(
-            513,1062,3868,4118,4147,4149,4156,4176
+            3698
         );
         
         foreach ($responsesss as $userdata) {

0fa331a12974f47f9902bb50c035d2db37c2d522
Md. Nurul Islam <webdev.nislam@gmail.com>
2015-07-28 13:36:21 +0000

    school page iframe

--- website/scripts/layouts/tdsfront/js/lib.js
+++ website/scripts/layouts/tdsfront/js/lib.js
@@ -116,7 +116,7 @@ $(document).ready(function () {
         hash = window.location.hash.substring(1);
         console.log('hash');
         $('iframe')[0].contentWindow.postMessage({"findElement": hash}, '*');
-        console.log($('iframe')[0]);
+        console.log($('iframe').length);
     }
 
     $(window).on('hashchange', sendHash);

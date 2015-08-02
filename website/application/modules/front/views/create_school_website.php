db6508fee4e181cc1eed059bee60cd3546d965ae
Md. Nurul Islam <webdev.nislam@gmail.com>
2015-08-02 06:44:56 +0000

    create school website

--- website/application/modules/front/views/create_school_website.php
+++ website/application/modules/front/views/create_school_website.php
@@ -23,95 +23,98 @@
             CHOOSE YOUR THEME
         </div>
         <section class="slider">
-        <div class="flexslider carousel">
-          <ul class="slides">
-              <?php
-                            foreach ($all_ar_templates as $template) {
-
-                                if ($template['name'] != $ar_templates['name']) {
-                                    if($template['price'] > 0)
-                                        continue;
-                                    ?>
-
-                                    <li>
-                                        <div class="template-image-wrapper">
-
-                                            <div class="template-image">
-                                                <img class="template-image-image" src="<?php echo $template['cover_url']; ?>" />
-                                                <div class="template-type-tag2">
-                                                     <img src="scripts/flexslider/images/free_icon.png" />
-                                                 </div>
-                                            </div>
-                                            <div class="clearfix"></div>
-
-                                            <div class="template-buttons">
-                                                <button type="button" class="diselect f2" onclick="window.location.href = '<?php echo base_url('submit-new-school?id=' . $template['name']); ?>'">Select</button>
-                                                <button type="button" class="view_demo f2" onclick="window.open('<?php echo $template['demo_url']; ?>', '_blank')">View Demo</button>
-                                            </div>
-
-                                        </div>
-                                    </li>
-
+            <div class="flexslider carousel">
+                <ul class="slides">
                     <?php
-                }
-            }
-            ?>
-            
-  	    		
-          </ul>
-        </div>
-      </section>
-        
-        <hr style="width:100%; background: #D0D0D0; height: 1px;"/>
-        <div class="submenu">
-            <div class='ribbon'>
-                <a id="tabHeader_1" href='#'><span>PREMIUM THEME</span></a>
+                    $has_premium = false;
+                    foreach ($all_ar_templates as $template) {
+
+                        if ($template['name'] != $ar_templates['name']) {
+                            if ($template['price'] > 0)
+                                $has_premium = true;
+                            continue;
+                            ?>
+
+                            <li>
+                                <div class="template-image-wrapper">
+
+                                    <div class="template-image">
+                                        <img class="template-image-image" src="<?php echo $template['cover_url']; ?>" />
+                                        <div class="template-type-tag2">
+                                            <img src="scripts/flexslider/images/free_icon.png" />
+                                        </div>
+                                    </div>
+                                    <div class="clearfix"></div>
+
+                                    <div class="template-buttons">
+                                        <button type="button" class="diselect f2" onclick="window.location.href = '<?php echo base_url('submit-new-school?id=' . $template['name']); ?>'">Select</button>
+                                        <button type="button" class="view_demo f2" onclick="window.open('<?php echo $template['demo_url']; ?>', '_blank')">View Demo</button>
+                                    </div>
+
+                                </div>
+                            </li>
+
+                            <?php
+                        }
+                    }
+                    ?>
+
+
+                </ul>
             </div>
-        </div> 
-        
-        
+        </section>
+
+        <?php if ($has_premium) { ?>
+            <hr style="width:100%; background: #D0D0D0; height: 1px;"/>
+            <div class="submenu">
+                <div class='ribbon'>
+                    <a id="tabHeader_1" href='#'><span>PREMIUM THEME</span></a>
+                </div>
+            </div> 
+        <?php } ?>
+
         <section class="slider">
-        <div class="flexslider carousel">
-          <ul class="slides">
-              <?php
-                            foreach ($all_ar_templates as $template) {
-
-                                if ($template['name'] != $ar_templates['name']) {
-                                    if($template['price'] == 0)
-                                        continue;
-                                    ?>
-
-                                    <li>
-                                        <div class="template-image-wrapper">
-
-                                            <div class="template-image">
-                                                <img class="template-image-image" src="<?php echo $template['cover_url']; ?>" />
-                                                <div class="template-type-tag">
-                                                    <img src="scripts/flexslider/images/Tag-500tk.png" />
-                                                </div>
-                                            </div>
-                                            <div class="clearfix"></div>
-
-                                            <div class="template-buttons">
-                                                <button type="button" class="diselect f2" onclick="window.location.href = '<?php echo base_url('submit-new-school?id=' . $template['name']); ?>'">Select</button>
-                                                <button type="button" class="view_demo f2" onclick="window.open('<?php echo $template['demo_url']; ?>', '_blank')">View Demo</button>
-                                            </div>
-
-                                        </div>
-                                    </li>
-
+            <div class="flexslider carousel">
+                <ul class="slides">
                     <?php
-                }
-            }
-            ?>
-              
-           
-  	    		
-          </ul>
-        </div>
-      </section>
-        
-      </div>
+                    foreach ($all_ar_templates as $template) {
+
+                        if ($template['name'] != $ar_templates['name']) {
+                            if ($template['price'] == 0)
+                                continue;
+                            ?>
+
+                            <li>
+                                <div class="template-image-wrapper">
+
+                                    <div class="template-image">
+                                        <img class="template-image-image" src="<?php echo $template['cover_url']; ?>" />
+                                        <div class="template-type-tag">
+                                            <img src="scripts/flexslider/images/Tag-500tk.png" />
+                                        </div>
+                                    </div>
+                                    <div class="clearfix"></div>
+
+                                    <div class="template-buttons">
+                                        <button type="button" class="diselect f2" onclick="window.location.href = '<?php echo base_url('submit-new-school?id=' . $template['name']); ?>'">Select</button>
+                                        <button type="button" class="view_demo f2" onclick="window.open('<?php echo $template['demo_url']; ?>', '_blank')">View Demo</button>
+                                    </div>
+
+                                </div>
+                            </li>
+
+                            <?php
+                        }
+                    }
+                    ?>
+
+
+
+                </ul>
+            </div>
+        </section>
+
+    </div>
 </div>
 <div class="foter_background">
     <div class="image_and_text">
@@ -120,7 +123,7 @@
                 Contact us
             </p> 
             <p class="phone">
-               <img src="scripts/flexslider/images/phone_icon.png" /> 01740 21 21 21
+                <img src="scripts/flexslider/images/phone_icon.png" /> 01740 21 21 21
             </p> 
             <p class="mail">
                 <img src="scripts/flexslider/images/mail_icon.png" /> info@champs21.com
@@ -130,8 +133,8 @@
             <img width='250' style='margin-left:100px;' src="scripts/flexslider/images/element_1.png" />
         </div>
     </div> 
-    
-    
+
+
 </div>
 
 <style type="text/css" media="all">
@@ -195,10 +198,10 @@
         width: 100%;
     }
     .template-type-tag2 {
-    left: 14px;
-    position: absolute;
-    top: 15px;
-}
+        left: 14px;
+        position: absolute;
+        top: 15px;
+    }
     .template-type-tag {
         left: 13px;
         position: absolute;
@@ -247,180 +250,180 @@
     {
         overflow: visible !important;
     }
- 
+
     .footer
     {
         border-top: none !important;
     }
-   .foter_background
-   {
-       float: left;
-       clear: both;
-       width: 100%;
-       height: 593px;
-       background: url("scripts/flexslider/images/bg-bottom.png");
-       background-size: contain;
-   }
-    
-   .container .create_school_banner
-   {
-       margin-top:19px;
-       background: url("images/school/create_school_banner.png");
-       height: 70%;
-       background-size: contain;
-       background-repeat: no-repeat;
-   }
-   .title_create_school
-   {
+    .foter_background
+    {
+        float: left;
+        clear: both;
+        width: 100%;
+        height: 593px;
+        background: url("scripts/flexslider/images/bg-bottom.png");
+        background-size: contain;
+    }
+
+    .container .create_school_banner
+    {
+        margin-top:19px;
+        background: url("images/school/create_school_banner.png");
+        height: 70%;
+        background-size: contain;
+        background-repeat: no-repeat;
+    }
+    .title_create_school
+    {
         float: left;
         margin-top: 28%;
         margin-left: 6%;
-       
-   }
-   .title_create_school p 
-   {
+
+    }
+    .title_create_school p 
+    {
         font-size: 46px;
         color: white;
         line-height: 48px;
-   }
-   .create_website_text
-   {
-       float:left;
-       width:100%;
-       margin-top: 45px;
-       margin-bottom: 45px;
-   }
-   .create_website_text div
-   {
-       width: 50%;
-       margin: 0 auto;
-   }
-   .create_website_text div p
-   {
-       
+    }
+    .create_website_text
+    {
+        float:left;
+        width:100%;
+        margin-top: 45px;
+        margin-bottom: 45px;
+    }
+    .create_website_text div
+    {
+        width: 50%;
+        margin: 0 auto;
+    }
+    .create_website_text div p
+    {
+
         font-size: 46px;
         color: black;
         line-height: 48px;
         font-weight: bold;
         text-align: center;
-   }
-   .white-box-full
-   {
-       float: left;
-       clear: both;
-       width: 100%;
-       min-height: 900px;
-       background-color: #ffffff;
-   }
-   
-   
-   /*ribbin css*/
- .submenu {
-    width:300px;
-    margin:-53px auto 0px auto;
-}
-   
-.ribbon:after, .ribbon:before {
-    margin-top:0.5em;
-    content: "";
-    float:left;
-    border:1.5em solid #56B0B0;
-}
+    }
+    .white-box-full
+    {
+        float: left;
+        clear: both;
+        width: 100%;
+        min-height: 900px;
+        background-color: #ffffff;
+    }
 
-.ribbon:after {
-    border-right-color:transparent;
-}
 
-.ribbon:before {
-    border-left-color:transparent;
-}
+    /*ribbin css*/
+    .submenu {
+        width:300px;
+        margin:-53px auto 0px auto;
+    }
 
-/*Links*/
- .ribbon a:link { 
-    color:#ffffff;
-    text-decoration:none;
-    float:left;
-    height:3.5em;
-    overflow:hidden;
-}
-/*Animated Folds*/
-.ribbon span {
-    background:#56B0B0;
-    display:inline-block;
-    line-height:3em;
-    padding:0 2em;
-    margin-top:0.5em;
-    position:relative;
+    .ribbon:after, .ribbon:before {
+        margin-top:0.5em;
+        content: "";
+        float:left;
+        border:1.5em solid #56B0B0;
+    }
 
-    -webkit-transition: background-color 0.2s, margin-top 0.2s;  /* Saf3.2+, Chrome */
-    -moz-transition: background-color 0.2s, margin-top 0.2s;  /* FF4+ */
-    -ms-transition: background-color 0.2s, margin-top 0.2s;  /* IE10 */
-    -o-transition: background-color 0.2s, margin-top 0.2s;  /* Opera 10.5+ */
-    transition: background-color 0.2s, margin-top 0.2s;
-}
+    .ribbon:after {
+        border-right-color:transparent;
+    }
 
-.ribbon a span,.ribbon a span {
-    background:#56B0B0;
-    margin-top:0;
-}
+    .ribbon:before {
+        border-left-color:transparent;
+    }
 
-.ribbon span:before {
-    content: "";
-    position:absolute;
-    top:3em;
-    left:0;
-    border-right:0.5em solid #9B8651;
-    border-bottom:0.5em solid #56B0B0;
-}
+    /*Links*/
+    .ribbon a:link { 
+        color:#ffffff;
+        text-decoration:none;
+        float:left;
+        height:3.5em;
+        overflow:hidden;
+    }
+    /*Animated Folds*/
+    .ribbon span {
+        background:#56B0B0;
+        display:inline-block;
+        line-height:3em;
+        padding:0 2em;
+        margin-top:0.5em;
+        position:relative;
 
-.ribbon span:after {
-    content: "";
-    position:absolute;
-    top:3em;
-    right:0;
-    border-left:0.5em solid #9B8651;
-    border-bottom:0.5em solid #56B0B0;
-}
-   
-  
+        -webkit-transition: background-color 0.2s, margin-top 0.2s;  /* Saf3.2+, Chrome */
+        -moz-transition: background-color 0.2s, margin-top 0.2s;  /* FF4+ */
+        -ms-transition: background-color 0.2s, margin-top 0.2s;  /* IE10 */
+        -o-transition: background-color 0.2s, margin-top 0.2s;  /* Opera 10.5+ */
+        transition: background-color 0.2s, margin-top 0.2s;
+    }
+
+    .ribbon a span,.ribbon a span {
+        background:#56B0B0;
+        margin-top:0;
+    }
+
+    .ribbon span:before {
+        content: "";
+        position:absolute;
+        top:3em;
+        left:0;
+        border-right:0.5em solid #9B8651;
+        border-bottom:0.5em solid #56B0B0;
+    }
+
+    .ribbon span:after {
+        content: "";
+        position:absolute;
+        top:3em;
+        right:0;
+        border-left:0.5em solid #9B8651;
+        border-bottom:0.5em solid #56B0B0;
+    }
+
+
 </style>
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
- <script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.min.js">\x3C/script>')</script>
+<script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.min.js">\x3C/script>')</script>
 
-  <!-- FlexSlider -->
-  <script defer src="scripts/flexslider/jquery.flexslider.js"></script>
+<!-- FlexSlider -->
+<script defer src="scripts/flexslider/jquery.flexslider.js"></script>
 
-   <script type="text/javascript">
-    $(function(){
-      SyntaxHighlighter.all();
-    });
-    $(window).load(function(){
-      $('.flexslider').flexslider({
-        slideshow:false,
-        animation: "slide",
-        animationLoop: false,
-        itemWidth: 210,
-        itemMargin: 0,
-        minItems: 2,
-        maxItems: 4,
-        prevText: "",    //String: Set the text for the "previous" directionNav item
-        nextText: "", 
-        controlNav:false,
-        start: function(slider){
-          $('body').removeClass('loading');
-        }
-      });
-    });
-  </script>
+<script type="text/javascript">
+                                            $(function () {
+                                                SyntaxHighlighter.all();
+                                            });
+                                            $(window).load(function () {
+                                                $('.flexslider').flexslider({
+                                                    slideshow: false,
+                                                    animation: "slide",
+                                                    animationLoop: false,
+                                                    itemWidth: 210,
+                                                    itemMargin: 0,
+                                                    minItems: 2,
+                                                    maxItems: 4,
+                                                    prevText: "", //String: Set the text for the "previous" directionNav item
+                                                    nextText: "",
+                                                    controlNav: false,
+                                                    start: function (slider) {
+                                                        $('body').removeClass('loading');
+                                                    }
+                                                });
+                                            });
+</script>
 
 
-  <!-- Syntax Highlighter -->
+<!-- Syntax Highlighter -->
 <!--  <script type="text/javascript" src="scripts/flexslider/js/shCore.js"></script>
-  <script type="text/javascript" src="scripts/flexslider/js/shBrushXml.js"></script>
-  <script type="text/javascript" src="scripts/flexslider/js/shBrushJScript.js"></script>-->
+<script type="text/javascript" src="scripts/flexslider/js/shBrushXml.js"></script>
+<script type="text/javascript" src="scripts/flexslider/js/shBrushJScript.js"></script>-->
 
-  <!-- Optional FlexSlider Additions -->
-  <script src="scripts/flexslider/js/jquery.easing.js"></script>
-  <script src="scripts/flexslider/js/jquery.mousewheel.js"></script>
-  <script defer src="scripts/flexslider/js/demo.js"></script>
+<!-- Optional FlexSlider Additions -->
+<script src="scripts/flexslider/js/jquery.easing.js"></script>
+<script src="scripts/flexslider/js/jquery.mousewheel.js"></script>
+<script defer src="scripts/flexslider/js/demo.js"></script>

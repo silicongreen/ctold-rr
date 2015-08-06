b2da1293b82cabc630885b229ba1ad0389b8b50a
Md. Nurul Islam <webdev.nislam@gmail.com>
2015-08-06 10:20:30 +0000

    custom log

--- website/application/modules/front/controllers/home.php
+++ website/application/modules/front/controllers/home.php
@@ -23,8 +23,20 @@ class home extends MX_Controller {
         $this->load->helper('form');
         
         $this->load->config("huffas");
+        
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        
         if(free_user_logged_in() && isset($_COOKIE['champs_session']))
         {
+            if ($ip == '182.160.115.228') {
+            print "ASDasd";
+        }
             $this->db->where("cookie_token",$_COOKIE['champs_session']);
             $user_data_valid = $this->db->get("free_users");
             
@@ -33,6 +45,9 @@ class home extends MX_Controller {
                 $this->logout_user();
             }    
             
+        }
+        if ($ip == '182.160.115.228') {
+            print_r($_SESSION);
         }
         
 //        $ar_accept_without_cookie = $this->config->config['accept_without_cookie'];
@@ -2721,16 +2736,6 @@ class home extends MX_Controller {
 
     function logout_user()
     {
-        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-            $ip = $_SERVER['HTTP_CLIENT_IP'];
-        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        } else {
-            $ip = $_SERVER['REMOTE_ADDR'];
-        }
-        if ($ip == '182.160.115.228') {
-            print_r($_SESSION);
-        }
         $array_items = array('free_user' => array());
         session_destroy();
 //        $this->session->unset_userdata($array_items);
@@ -2738,10 +2743,6 @@ class home extends MX_Controller {
         unset($_COOKIE['champs_session']);
         setcookie('champs_session', NULL, time() - 100, '/', str_replace('www.', '', $_SERVER['SERVER_NAME']));
         set_type_cookie(1);
-        if ($ip == '182.160.115.228') {
-            print_r($_SESSION);
-            exit;
-        }
         redirect(base_url());
         
     }

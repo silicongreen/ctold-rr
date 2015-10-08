50eccad4ac3fff30232c94a72c321f81fc850f03
unknown <rlikhon@gmail.com>
2015-10-08 13:57:11 +0600

    single page edit 3

--- website/application/widgets/singlepage_postbox/view.php
+++ website/application/widgets/singlepage_postbox/view.php
@@ -138,4 +138,59 @@
 	.postlist-tab .tab-content{padding:0px;}
 	.postlist-tab .media-heading-p{color:#000;font-family:sans-serif;}
 	.postlist-tab .media-heading-p a{color:#000;font-size:15px;}
+        
+        .media {
+  margin-top: 0px;
+  padding:15px;
+  border-bottom: 1px solid #ccc;
+}
+.media:first-child {
+  margin-top: 0;
+}
+.media,
+.media-body {
+  overflow: hidden;
+  zoom: 1;
+}
+.media:hover{
+	background-color:#F4F8F9;
+	cursor: pointer;
+}
+.media-body {
+  width: 10000px;
+}
+.media-object {
+  display: block;
+}
+.media-object.img-thumbnail {
+  max-width: none;
+}
+.media-right,
+.media > .pull-right {
+  padding-left: 10px;
+}
+.media-left,
+.media > .pull-left {
+  padding-right: 10px;
+}
+.media-left,
+.media-right,
+.media-body {
+  display: table-cell;
+  vertical-align: top;
+}
+.media-middle {
+  vertical-align: middle;
+}
+.media-bottom {
+  vertical-align: bottom;
+}
+.media-heading {
+  margin-top: 0;
+  margin-bottom: 5px;
+}
+.media-list {
+  padding-left: 0;
+  list-style: none;
+}
 </style>

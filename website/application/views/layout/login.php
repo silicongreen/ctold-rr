5c3de6f57711c4885bb3368f965a74e6c8ea101a
fahim mohammad chowdhury <fahim.mohammad@teamworkbd.com>
2015-12-01 10:42:32 +0000

    login design change

--- website/application/views/layout/login.php
+++ website/application/views/layout/login.php
@@ -1,5 +1,5 @@
 <li id="login_button_classune"><a style="color:#000;text-decoration:none;" id="classtune_a"  href="javascript:void(0)"><span>Login</span></a>
-    <div id="login_form_classune" class="col-md-3" style="overflow: hidden; width:22%;">
+    <div id="login_form_classune" class="col-md-3" style="overflow: hidden; width:23%;">
         
         <div class="col-md-12">
             <div class="row-fluid">

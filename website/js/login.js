6961be9d0780b47475052bafb18fe3c84131e6ac
fahim mohammad chowdhury <fahim.mohammad@teamworkbd.com>
2015-12-01 08:41:31 +0000

    login design change

--- website/js/login.js
+++ website/js/login.js
@@ -12,33 +12,41 @@ $(document).on('click', '#login_button_classune a#classtune_a', function () {
     else {
         $("#login_form_classune").slideUp("slow");
         $("#login_button_classune").removeClass('act_login');
+        $("#form_login_classtune span.legend").html("");
     }
 });
+$(document).click(function(e) {
+        if (!$(e.target).is('#login_button_classune, #login_button_classune *')) {
+            $("#login_form_classune").slideUp("slow");
+            $("#login_button_classune").removeClass('act_login');
+            $("#form_login_classtune span.legend").html("");
+        }
+ });
 $(document).on('click', '#form_login_classtune button#submit', function (e) {
     e.preventDefault();
     if ($("#form_login_classtune #username").val() == "")
     {
-        $("#form_login_classtune span.legend").html("<span class='error'>Username can't be empty</span>");
+        $("#form_login_classtune span.legend").html("<div class='alert alert-danger'><strong>Username</strong> can't be empty</div>");
         $("#form_login_classtune span.legend .error").show("slow");
     }
     else if ($("#form_login_classtune #password").val() == "")
     {
-        $("#form_login_classtune span.legend").html("<span class='error'>Password can't be empty</span>");
+        $("#form_login_classtune span.legend").html("<div class='alert alert-danger'><strong>Password</strong> can't be empty</div>");
         $("#form_login_classtune span.legend .error").show("slow");
     }
     else
     {
-        $("#form_login_classtune span.legend").html("loading...");
+        $("#form_login_classtune span.legend").html("<div class='alert alert-info'><strong>Loading......</strong></div>");
         $.post("/login", {username: $("#form_login_classtune #username").val(), password: $("#form_login_classtune #password").val()})
                 .done(function (data) {
                     if (data == "0")
                     {
-                        $("#form_login_classtune span.legend").html("<span class='error'>Wrong Username or Password</span>");
+                        $("#form_login_classtune span.legend").html("<div class='alert alert-danger'>Wrong <strong>Username</strong> or <strong>Password</strong></div>");
                         $("#form_login_classtune span.legend .error").show("slow");
                     }
                     else
                     {
-                        $("#form_login_classtune span.legend").html("Redirecting to your school...");
+                        $("#form_login_classtune span.legend").html("<div class='alert alert-info'><strong>Redirecting to your school...</strong></div>");
                         location.href = data;
                     }
 

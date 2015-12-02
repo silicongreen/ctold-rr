98e72e45d6ede4a501c31c33fd90e0ec73699aa7
fahim mohammad chowdhury <fahim.mohammad@teamworkbd.com>
2015-12-02 11:54:36 +0000

    mail sending

--- website/application/views/layout/header.php
+++ website/application/views/layout/header.php
@@ -1,15 +1,9 @@
-
 <!DOCTYPE html>
 <html lang="de" xmlns="http://www.w3.org/1999/xhtml">
     <head>
 
         <meta charset="utf-8" />
-        <!-- 
-                This website is powered by TYPO3 - inspiring people to share!
-                TYPO3 is a free open source Content Management Framework initially created by Kasper Skaarhoj and licensed under GNU/GPL.
-                TYPO3 is copyright 1998-2012 of Kasper Skaarhoj. Extensions are copyright of their respective owners.
-                Information and contribution at http://typo3.org/
-        -->
+     
 
         <base href="<?php base_url()?>" />
         
@@ -20,7 +14,7 @@
         <meta name="generator" content="CHAMPS21" />
         <meta name="language" content="en" />
         <meta name="description" content="Complete School Management System" />
-        <meta name="keywords" content="Champs21,School,Education,Management,School Management System" />
+        <meta name="keywords" content="Champs21,classtune,teacher,parent,student,guardian,child,School,Education,Management,School Management System" />
         <meta name="robots" content="index, follow" />
         <meta name="author" content="CLASSTUNE" />
         
@@ -29,11 +23,8 @@
 
 
         <meta http-equiv="content-language" content="en" />
-        <!-- Proxy: Datei nicht auf Proxy-Server speichern //-->
         <meta http-equiv="pragma" content="no-cache" />
         <meta http-equiv="expires" content="0" />
-
-        <!-- Browser: keinen Cache benutzen, sondern von Originalseite laden //-->
         <meta http-equiv="cache-control" content="no-cache" />
         <meta name="viewport" content="width=1200, height=900" />
 

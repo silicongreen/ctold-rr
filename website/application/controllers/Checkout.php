6762c665ec767cd0d835424f0f930643a8b5bb5a
Huffas Abdullah <huffas@gmail.com>
2015-12-06 13:03:36 +0000

    2Checkout Payment Implementation fix

--- website/application/controllers/Checkout.php
+++ website/application/controllers/Checkout.php
@@ -233,17 +233,15 @@ class Checkout extends CI_Controller {
                     try {
                         $a_request = array(
                             "sellerId" => $this->config->config['PaymentParams']['2Checkout']['sellerID'],
-                            "merchantOrderId" => uniqid(),
+                            "merchantOrderId" => mt_rand(),
                             "token" => $_POST['token_request'],
                             "currency" => 'USD',
                             "total" => $no_of_student * $unit_price,
                             'recurrence'    => $this->config->config['PaymentRules']['recurrence_unit'] . " " . $this->config->config['PaymentRules']['recurrence_type']
                         );
-                        print_r($a_request);
-                        exit;
+                        
                         $o_charge = Twocheckout_Charge::auth($a_request);
-                        print_r($o_charge);
-                        exit;
+                        
                         if ( $o_charge['response']['responseCode'] == 'APPROVED' )
                         {
                             $data = $this->paidSchoolProcess($i_tmp_school_creation_data_id, $i_tmp_free_user_data_id);

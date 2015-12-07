37e219eb3db09bfac6db6873224d818eb4903841
Huffas Abdullah <huffas@gmail.com>
2015-12-07 10:56:31 +0000

    2Checkout Payment Implementation fix

--- website/application/controllers/Checkout.php
+++ website/application/controllers/Checkout.php
@@ -234,7 +234,7 @@ class Checkout extends CI_Controller {
                     //try {
                         $a_request = array(
                             "sellerId" => $this->config->config['PaymentParams']['2Checkout']['sellerID'],
-                            "merchantOrderId" => "2232",
+                            "merchantOrderId" => "22232",
                             "token" => $_POST['token_request'],
                             "currency" => 'USD',
                             "total"         => "0.99",

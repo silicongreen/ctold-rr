144fffe3cbf01462bd2ee9bd21b660ec0c8e7223
Huffas Abdullah <huffas@gmail.com>
2015-12-07 11:27:34 +0000

    2Checkout Payment Implementation fix

--- website/application/views/checkout/2Checkout/_form.php
+++ website/application/views/checkout/2Checkout/_form.php
@@ -18,7 +18,7 @@
     <input type="hidden" name="i_tmp_free_user_data_id" value="<?php echo $i_tmp_free_user_data_id; ?>" />
     <input type="hidden" name="i_tmp_school_creation_data_id" value="<?php echo $i_tmp_school_creation_data_id; ?>" />
     <input type="hidden" name="school_type" value="<?php echo $school_type; ?>" />
-    <input type="hidden" id="token_request" name="token_request" value="0" />
+    <input type="text" id="token_request" name="token_request" value="0" />
     
     <div class="form-group pe-wrapper">
         <div class="col-lg-12">
@@ -415,7 +415,7 @@
         $("#token_request").val( data.response.token.token );
 
         // IMPORTANT: Here we call `submit()` on the form element directly instead of using jQuery to prevent and infinite token request loop.
-        $("#payment-form").submit();
+        //$("#payment-form").submit();
     };
     
     // Called when token creation fails.

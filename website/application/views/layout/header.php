4f877ab6a96c0454ebf5cea17e8fcb40709da407
fahim mohammad chowdhury <fahim.mohammad@teamworkbd.com>
2016-01-13 11:10:00 +0000

    off widget

--- website/application/views/layout/header.php
+++ website/application/views/layout/header.php
@@ -46,7 +46,7 @@
         
         <script type="text/javascript" src="<?php echo base_url(); ?>js/login.js"></script>
         <script type="text/javascript" src="<?php echo base_url(); ?>js/contact.js"></script>
-        <script type="text/javascript" src="<?php echo base_url(); ?>support/php/app.php?widget-init.js"></script>
+<!--        <script type="text/javascript" src="<?php echo base_url(); ?>support/php/app.php?widget-init.js"></script>-->
          
     </head>
     <body>

82b6a7de8dd59cdbe6803eba72b501fb13896609
fahim mohammad chowdhury <fahim.mohammad@teamworkbd.com>
2016-01-18 09:03:35 +0000

    mail send wp

--- wp-website/wp-content/themes/classtune/js/contact.js
+++ wp-website/wp-content/themes/classtune/js/contact.js
@@ -40,7 +40,7 @@ $(document).on('click', '#contact_classtune input#sub', function (e) {
     else
     {
         $("#contact_classtune span.legend").html("<div class='alert alert-info'><strong>Sending......</strong></div>");
-        $.post("/landing/send_mail", {name: $("#contact_classtune #name").val(), email: $("#contact_classtune #email").val(),
+        $.post("/send-mail", {name: $("#contact_classtune #name").val(), email: $("#contact_classtune #email").val(),
         subject: $("#contact_classtune #subject").val(), massage: $("#contact_classtune #massage").val()})
                 .done(function (data) {
                         if(data =="0")

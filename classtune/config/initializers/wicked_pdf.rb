5a9336fd8c318a041b94006f7a80f68c1e347b49
fahim mohammad chowdhury <fahim.mohammad@teamworkbd.com>
2016-04-20 14:01:08 +0000

    report card akmis

--- classtune/config/initializers/wicked_pdf.rb
+++ classtune/config/initializers/wicked_pdf.rb
@@ -1,7 +1,7 @@
 WickedPdf.config = {
     :wkhtmltopdf => '/opt/wkhtmltopdf-amd64',
     :layout => "pdf.html",
-    :margin => {    :top=> 40,
+    :margin => {    :top=> 50,
                     :bottom => 20,
                     :left=> 10,
                     :right => 10},

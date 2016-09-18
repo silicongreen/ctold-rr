5ae5781f7b2829f6c505441fa7a262ce373c7f92
fahim mohammad chowdhury <fahim.mohammad@teamworkbd.com>
2016-09-18 11:42:56 +0000

    elective group problem

--- classtune/app/controllers/student_controller.rb
+++ classtune/app/controllers/student_controller.rb
@@ -2361,6 +2361,7 @@ class StudentController < ApplicationController
     
     @tmp_subject_to_test = Subject.find_by_code_and_batch_id(@subjects_code, b)
     unless @tmp_subject_to_test.nil?
+      abort(@tmp_subject_to_test.id)
       appropriate_elective_subject_id = @tmp_subject_to_test.id
     end
     

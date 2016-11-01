7ef3bc598a30a921440cda257cfd90839f1e999b
fahim mohammad chowdhury <fahim.mohammad@teamworkbd.com>
2016-11-01 11:59:40 +0000

    card attendance implemented

--- classtune/app/controllers/employee_attendance_controller.rb
+++ classtune/app/controllers/employee_attendance_controller.rb
@@ -294,7 +294,7 @@ class EmployeeAttendanceController < ApplicationController
     end
   end
   def report_card_generate
-    if params[:month].blank? and params[:year].blank? and params[:employee_id].blank?
+    if !params[:month].blank? and !params[:year].blank? and !params[:employee_id].blank?
       @employee = Employee.find(params[:employee_id])
       @month = params[:month]
       @year = params[:year]

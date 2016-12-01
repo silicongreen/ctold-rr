db2297e3ef06b191cfeb8c855eb80170f5b00a36
fahim mohammad chowdhury <fahim.mohammad@teamworkbd.com>
2016-12-01 07:12:20 +0000

    bf shahin result

--- classtune/app/controllers/exam_controller.rb
+++ classtune/app/controllers/exam_controller.rb
@@ -2265,10 +2265,11 @@ class ExamController < ApplicationController
     if MultiSchool.current_school.id == 246
       render :pdf => 'generated_report5_pdf',
         :orientation => 'Landscape', :zoom => 1.00
-    elsif MultiSchool.current_school.id == 319
+    elsif MultiSchool.current_school.id == 319 or MultiSchool.current_school.id == 2
       render :pdf => 'generated_report5_pdf',
         :orientation => 'Portrait', :zoom => 1.00
     else
+    
       render :pdf => 'generated_report5_pdf',
         :orientation => 'Landscape', :zoom => 1.00
     end

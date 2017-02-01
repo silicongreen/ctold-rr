869ed10ce345c4515c57f9983337a18afe0774ca
Rezoanul Arefin <rlikhon@rlikhon.com>
2017-02-01 10:17:59 +0000

    ctunr

--- wp-website/wp-content/plugins/user_contacts/user_contact.php
+++ wp-website/wp-content/plugins/user_contacts/user_contact.php
@@ -31,7 +31,7 @@ add_action('wp_ajax_send_mail_classtune2', 'send_mail_classtune2');
 add_action('wp_enqueue_scripts', 'rsvp_video_background_frontend_script');*/
 
 
-wp_enqueue_script('ajax-script', plugin_dir_url(__FILE__) . 'js/user_contact.js');  
+//wp_enqueue_script('ajax-script', plugin_dir_url(__FILE__) . 'js/user_contact.js');  
  wp_localize_script( 'ajax-script', 'contact_lol', array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
 
 if (!function_exists('send_mail_classtune2')) {

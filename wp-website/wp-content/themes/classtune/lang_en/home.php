45f3afa3ce2e1518859cb757c618f163b1ac8679
Rezoanul Arefin <rlikhon@rlikhon.com>
2017-02-02 12:08:43 +0000

    home

--- wp-website/wp-content/themes/classtune/lang_en/home.php
+++ wp-website/wp-content/themes/classtune/lang_en/home.php
@@ -28,7 +28,7 @@ get_header(); ?>
                                                 <div class="row">
                                                     <div class="col-md-12">
                                                         <div class="col-md-6">                                                            
-                                                            <video autoplay loop muted style="width: 700px;margin-top:95px;margin-left:95px;"><source src="http://www.dhakawall.com/dhakawall/ClassTune.mp4" type="video/mp4"></video>
+                                                            <video autoplay loop muted style="width:100%;margin-top:95px;"><source src="http://www.dhakawall.com/dhakawall/ClassTune.mp4" type="video/mp4"></video>
                                                         </div>
                                                         <div class="col-md-6">
                                                             <div class="tx-pxcontentboxes-pi1">

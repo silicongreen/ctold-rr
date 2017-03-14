925038096fde209c08e47a0e7a5ed49c6c7c5cb5
fahim <fahim.mohammad@teamworkbd.com>
2017-03-14 11:06:48 +0600

    continues result

--- src/api/protected/modules/api/controllers/ReportController.php
+++ src/api/protected/modules/api/controllers/ReportController.php
@@ -190,16 +190,16 @@ class ReportController extends Controller
         {
 
             $groupexam = new GroupedExams();
-            //$exam_report = $groupexam->getContinuesResult($batch_id,$connect_exam_id);
-            $attandence = new Attendances();
-            $adata = $attandence->getTotalPrsent($batch_id, $connect_exam_id,$exam_report['students']);
+            $exam_report = $groupexam->getContinuesResultTest($batch_id,$connect_exam_id);
+            //$attandence = new Attendances();
+            //$adata = $attandence->getTotalPrsent($batch_id, $connect_exam_id,$exam_report['students']);
 
 
             if ($exam_report) {
 
                 $response['data']['report'] = $exam_report;
-                $response['data']['total'] = $adata[0];
-                $response['data']['present_all'] = $adata[1];
+                //$response['data']['total'] = $adata[0];
+                //$response['data']['present_all'] = $adata[1];
                 $response['status']['code'] = 200;
                 $response['status']['msg'] = "EXAM_REPORT_FOUND";
             } else {

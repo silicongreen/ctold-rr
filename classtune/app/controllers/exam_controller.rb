4bb3bd19dfd1fc0d698a9dbe3bd6587f386535d5
fahim mohammad chowdhury <fahim.mohammad@teamworkbd.com>
2017-03-16 05:31:49 +0000

    add result domain to sfx

--- classtune/app/controllers/exam_controller.rb
+++ classtune/app/controllers/exam_controller.rb
@@ -2224,7 +2224,7 @@ class ExamController < ApplicationController
     if MultiSchool.current_school.id == 246
       render :pdf => 'continues',
         :orientation => 'Landscape', :zoom => 1.00
-    elsif MultiSchool.current_school.id == 319 or MultiSchool.current_school.id == 323 or MultiSchool.current_school.id == 325
+    elsif MultiSchool.current_school.id == 319 or MultiSchool.current_school.id == 323 or MultiSchool.current_school.id == 325 or MultiSchool.current_school.id == 324
       render :pdf => 'continues',
         :orientation => 'Portrait', :zoom => 1.00
     else
@@ -2594,7 +2594,7 @@ class ExamController < ApplicationController
     if MultiSchool.current_school.id == 246
       render :pdf => 'generated_report5_pdf',
         :orientation => 'Landscape', :zoom => 1.00
-    elsif MultiSchool.current_school.id == 319 or MultiSchool.current_school.id == 323 or MultiSchool.current_school.id == 325
+    elsif MultiSchool.current_school.id == 319 or MultiSchool.current_school.id == 323 or MultiSchool.current_school.id == 325 or MultiSchool.current_school.id == 324
       render :pdf => 'generated_report5_pdf',
         :orientation => 'Portrait', :zoom => 1.00
     else    

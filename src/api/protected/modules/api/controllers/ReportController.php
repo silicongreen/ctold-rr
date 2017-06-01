ac174b43f6dd39fea53fcb0c3eb3a6b6aa7016df
fahim <fahim.mohammad@teamworkbd.com>
2017-06-01 12:33:50 +0600

    exam problem

--- src/api/protected/modules/api/controllers/ReportController.php
+++ src/api/protected/modules/api/controllers/ReportController.php
@@ -326,7 +326,7 @@ class ReportController extends Controller
                 }
                 
                 if ($exam_report) {
-                    $response['data']['report'] = $new_connect_exam_id;
+                    $response['data']['exams'] = $new_connect_exam_id;
                     $response['data']['report'] = $exam_report;
                     $response['data']['connect_exams'] = $new_connect_exam_id;
                     $response['status']['code'] = 200;

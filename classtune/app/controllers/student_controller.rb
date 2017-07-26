89c9d6e521e7dd753404092d9ee6c83fde4b83cf
fahim mohammad chowdhury <fahim.mohammad@teamworkbd.com>
2017-07-26 13:02:41 +0000

    finana

--- classtune/app/controllers/student_controller.rb
+++ classtune/app/controllers/student_controller.rb
@@ -1701,7 +1701,7 @@ class StudentController < ApplicationController
     else
       @batch_id = params[:batch_id]
     end
-    @students = Student.active.find_all_by_batch_id(@batch_id, :order => 'first_name ASC')
+    @students = Student.active.find_all_by_batch_id(@batch_id, :order => 'cast(class_roll_no as unsigned) ASC,first_name ASC')
     render(:update) { |page| page.replace_html 'students', :partial => 'students_by_course' }
   end
   

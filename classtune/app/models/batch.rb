40c917f19494744d1074f4aee4af0dd6fda5c5e8
fahim mohammad chowdhury <fahim.mohammad@teamworkbd.com>
2017-08-21 13:17:30 +0000

    name wise sort

--- classtune/app/models/batch.rb
+++ classtune/app/models/batch.rb
@@ -23,7 +23,7 @@ class Batch < ActiveRecord::Base
   belongs_to :weekday_set
   belongs_to :class_timing_set
 
-  has_many :students
+  has_many :students, :order => 'students.first_name'
   has_many :grouped_exam_reports
   has_many :grouped_batches
   has_many :archived_students

17238a9e3f76bfcbdfebaf738f6c56c555aae80b
fahim mohammad chowdhury <fahim.mohammad@teamworkbd.com>
2017-08-24 13:27:08 +0000

    event change

--- classtune/app/controllers/exam_controller.rb
+++ classtune/app/controllers/exam_controller.rb
@@ -3820,7 +3820,7 @@ class ExamController < ApplicationController
             render :pdf => template,
             :save_to_file => file_name,
             :save_only    => for_save,
-            :orientation => 'Portrait' 
+            :orientation => 'Landscape' 
         elsif  MultiSchool.current_school.id == 340  
             if @connect_exam_obj.result_type == 13
               render :pdf => template,

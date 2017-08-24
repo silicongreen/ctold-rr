4a28119e447edd91a2a5aaa25625ae4938e6b6af
fahim mohammad chowdhury <fahim.mohammad@teamworkbd.com>
2017-08-24 13:56:41 +0000

    gn pdf

--- classtune/app/controllers/exam_controller.rb
+++ classtune/app/controllers/exam_controller.rb
@@ -3821,6 +3821,10 @@ class ExamController < ApplicationController
             :save_to_file => file_name,
             :save_only    => for_save,
             :orientation => 'Landscape',
+            :margin => {    :top=> 20,
+            :bottom => 10,
+            :left=> 10,
+            :right => 10},
             :footer => {:html => { :template=> 'layouts/pdf_empty_footer.html'}}
         elsif  MultiSchool.current_school.id == 340  
             if @connect_exam_obj.result_type == 13

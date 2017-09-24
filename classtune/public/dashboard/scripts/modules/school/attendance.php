ebe8a477ef9f95eb2e5d90cd3d9e9fc6030786bb
Huffas Abdullah <huffas@gmail.com>
2017-09-24 11:21:27 +0000

    Dashboard Classtune fixes the Count issue

--- classtune/public/dashboard/scripts/modules/school/attendance.php
+++ classtune/public/dashboard/scripts/modules/school/attendance.php
@@ -138,7 +138,6 @@
             $chart['data'][] = (int) ($sr['present'] + $sr['late']);
             //$i--;
         }
-        print_r($chart);
         $a_options = array("type" => "bar", "barColor" => $chart['color'], "barWidth" => "10%", "height" => "28px");
         $tmp['graph_attendance'] = "<span class='attendance_chart' sparkline data='" . json_encode($chart['data']) . "' options='" . json_encode($a_options) . "'  watch-me='numberOfPages'>
                                     </span>";

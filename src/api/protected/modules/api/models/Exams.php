b6a8d47a62155b1c5d8242faf5de30120db51a7f
fahim <fahim.mohammad@teamworkbd.com>
2017-09-27 14:03:01 +0600

    exam id added

--- src/api/protected/modules/api/models/Exams.php
+++ src/api/protected/modules/api/models/Exams.php
@@ -183,33 +183,18 @@ class Exams extends CActiveRecord
     }
     public function getrankedStudents($exam_group_ids)
     {
-        $criteria = new CDbCriteria();
-        $criteria->together = true;
-        $criteria->select = 't.id';
-        $criteria->addInCondition('t.exam_group_id',$exam_group_ids); 
-        
-        $criteria->with = array(
-                'Scores' => array(
-                    'select' => 'SUM(Scores.marks) AS total_score',
-                    'with' => array(
-                        'Students' => array(
-                            'select' => 'Students.*',
-                            
-                        )
-
-                    )
-                )
-        );
-        
-        $criteria->group = 'Scores.student_id';
-        $criteria->order = 'total_score DESC';
-        $students_ranked = $this->find($criteria);
-        
+        $sql = "SELECT SUM( Scores.marks ) AS total_score,students.*
+        FROM exams
+        LEFT JOIN exam_scores AS Scores ON Scores.exam_id = exams.id
+        LEFT JOIN students ON Scores.student_id = students.id
+        WHERE exams.exam_group_id IN (" . implode(",",$exam_group_ids) . ")
+        GROUP BY Scores.student_id
+        ORDER BY total_score DESC"; 
+        $data = $this->findAllBySql($sql);
         $students = array();
         foreach ($students_ranked as $value)
         {
-           $students[] = $value;
-            
+            $students[] = $value;
         }
         return $students;
     }        

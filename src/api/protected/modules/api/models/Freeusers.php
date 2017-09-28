64988bfcd19297839e596421809aa367b3a0d619
fahim <fahim.mohammad@teamworkbd.com>
2017-09-28 14:58:43 +0600

    free user email unique check off

--- src/api/protected/modules/api/models/Freeusers.php
+++ src/api/protected/modules/api/models/Freeusers.php
@@ -40,8 +40,7 @@ class Freeusers extends CActiveRecord {
         // NOTE: you should only define rules for those attributes that
         // will receive user inputs.
         return array(
-            array('user_type, email, password', 'required', 'on' => 'insert'),
-            array('email', 'unique'),
+            array('user_type, email, password', 'required', 'on' => 'insert')
         );
     }
 

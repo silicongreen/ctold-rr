15f61b8286dd460d495d931521adce75f5d83adf
fahim <fahim.mohammad@teamworkbd.com>
2017-10-09 12:42:21 +0600

    some api problems

--- src/api/protected/modules/api/models/OnlineExamGroups.php
+++ src/api/protected/modules/api/models/OnlineExamGroups.php
@@ -318,7 +318,7 @@ class OnlineExamGroups extends CActiveRecord {
 
                         $response_array['question'][$i]['id'] = $questions->id;
                         $response_array['question'][$i]['question'] = Settings::substr_with_unicode($questions->question);
-                        $response_array['question'][$i]['explanation'] = null;
+                        $response_array['question'][$i]['explanation'] = "";
                         if($questions->explanation)
                         {
                             $response_array['question'][$i]['explanation'] = $questions->explanation;

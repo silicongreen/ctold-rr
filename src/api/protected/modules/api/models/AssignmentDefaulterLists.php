dc7f8aaf659cd76f8199932eddb60e8d76174fe5
fahim <fahim.mohammad@teamworkbd.com>
2017-11-14 15:42:58 +0600

    school added

--- src/api/protected/modules/api/models/AssignmentDefaulterLists.php
+++ src/api/protected/modules/api/models/AssignmentDefaulterLists.php
@@ -128,12 +128,12 @@ class AssignmentDefaulterLists extends CActiveRecord
                     $i_loop = 0;
                     foreach($assignment_students as $svalue)
                     {
-                        $std_list[$i_loop]['student_id'] = $svalue.class_roll_no;
-                        $std_list[$i_loop]['student_name'] = $svalue.first_name." ".$svalue.first_name." ".$svalue.last_name;
+                        $std_list[$i_loop]['student_id'] = $svalue->class_roll_no;
+                        $std_list[$i_loop]['student_name'] = $svalue->first_name." ".$svalue->first_name." ".$svalue->last_name;
                         $std_list[$i_loop]['student_name'] = str_replace("  "," ", $std_list[$i]['student_name']);
-                        $std_list[$i_loop]['class_roll_no'] = $svalue.class_roll_no;
+                        $std_list[$i_loop]['class_roll_no'] = $svalue->class_roll_no;
                         $std_list[$i_loop]['defaulter'] = 0;
-                        if(in_array($svalue.id, $d_list))
+                        if(in_array($svalue->id, $d_list))
                         {
                             $std_list[$i_loop]['defaulter'] = 1;
                         }

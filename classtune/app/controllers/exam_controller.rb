c1b05462b231afe6aaddde671acbbcb02abe6f9c
fahim mohammad chowdhury <fahim.mohammad@teamworkbd.com>
2017-12-03 12:30:27 +0000

    cherry logo

--- classtune/app/controllers/exam_controller.rb
+++ classtune/app/controllers/exam_controller.rb
@@ -3916,7 +3916,7 @@ class ExamController < ApplicationController
             :save_to_file => file_name,
             :save_only    => for_save,
             :orientation => 'Landscape',
-            :margin => {:top=> 45,
+            :margin => {:top=> 30,
             :bottom => 40,
             :left=> 10,
             :right => 10}

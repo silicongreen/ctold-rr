2253b01407da3c394c3eb2eeaf355cdd2dc8ff12
fahim mohammad chowdhury <fahim.mohammad@teamworkbd.com>
2017-12-03 09:41:28 +0000

    cherry blossoms

--- classtune/app/controllers/exam_controller.rb
+++ classtune/app/controllers/exam_controller.rb
@@ -3915,7 +3915,11 @@ class ExamController < ApplicationController
           render :pdf => template,
             :save_to_file => file_name,
             :save_only    => for_save,
-            :orientation => 'Landscape'
+            :orientation => 'Landscape',
+            :margin => {:top=> 35,
+            :bottom => 40,
+            :left=> 10,
+            :right => 10}
         elsif MultiSchool.current_school.id == 319 or MultiSchool.current_school.id == 323 or MultiSchool.current_school.id == 325
           if MultiSchool.current_school.id == 319  and (@connect_exam_obj.result_type == 2 or @connect_exam_obj.result_type == 3 or @connect_exam_obj.result_type == 5 or @connect_exam_obj.result_type == 7)
             render :pdf => template,

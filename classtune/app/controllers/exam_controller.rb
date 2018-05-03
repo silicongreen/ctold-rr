0935f4ddc74d8966848bbdc1ffe20050084647b1
Fahim Mohammad Chowdhury <fahim.mohammad@teamworkbd.com>
2018-05-03 09:30:17 +0000

    pdf header

--- classtune/app/controllers/exam_controller.rb
+++ classtune/app/controllers/exam_controller.rb
@@ -4275,7 +4275,7 @@ class ExamController < ApplicationController
             :save_to_file => file_name,
             :save_only    => for_save,
             :orientation => 'Landscape',
-            :margin => {  :top=> 30,
+            :margin => {  :top=> 25,
             :bottom => 10,
             :left=> 10,
             :right => 10},

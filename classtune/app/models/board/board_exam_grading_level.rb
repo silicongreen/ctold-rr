87090d5f8794fa266493201fe6bd56fca4c83235
Fahim Mohammad Chowdhury <fahim.mohammad@teamworkbd.com>
2018-10-28 12:13:44 +0000

    some changes

--- classtune/app/models/board/board_exam_grading_level.rb
+++ classtune/app/models/board/board_exam_grading_level.rb
@@ -22,4 +22,40 @@ class BoardExamGradingLevel < ActiveRecord::Base
   validates_numericality_of :min_score ,:credit_points ,:greater_than_or_equal_to => 0, :message =>:must_be_positive ,:allow_blank=>true
   default_scope :order => 'min_score desc'
   belongs_to :board_exam
+  after_save :delete_cache
+  before_destroy :delete_cache 
+  
+  def delete_cache
+    keygradinglevel = "board_exam_grading_level_"
+    Rails.cache.delete_matched(/#{keygradinglevel}*/) 
+  end
+  def default(board_exam_id)
+      gradding_level = Rails.cache.fetch("board_exam_grading_level_#{board_exam_id}"){
+          grades = GradingLevel.find(:all,:conditions => { :board_exam_id => board_exam_id }, :order => 'min_score desc')
+          grades
+        }
+      gradding_level
+  end 
+  def percentage_to_grade(percent_score,board_exam_id)
+    grade = {}
+    grades = GradingLevel.default(board_exam_id)
+    grades.each do |gradevalue|
+      if gradevalue.min_score <= percent_score.round
+        grade = gradevalue
+        break
+      end  
+    end
+    grade
+  end
+  def grade_point_to_grade(grade_point, board_exam_id)
+    grade = {}
+    grades = GradingLevel.default(board_exam_id)
+    grades.each do |gradevalue|
+      if gradevalue.credit_points <= grade_point
+        grade = gradevalue
+        break
+      end  
+    end
+    grade
+  end
 end

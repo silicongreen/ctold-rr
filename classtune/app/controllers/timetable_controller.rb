db3153ee55d01ad863c5ba70f152814b654c4c70
Fahim Mohammad Chowdhury <fahim.mohammad@teamworkbd.com>
2018-11-26 12:51:18 +0000

    result problem

--- classtune/app/controllers/timetable_controller.rb
+++ classtune/app/controllers/timetable_controller.rb
@@ -859,7 +859,7 @@ class TimetableController < ApplicationController
         flash[:notice] = t('updated_with_errors')
       end
     end
-    @batches = Batch.active.paginate(:order=>"created_at DESC",:include=>[{:subjects=>:employees},:course], :page => params[:page], :per_page => 10)
+    @batches = Batch.active.paginate(:order=>"batches.name ASC, courses.course_name ASC, courses.section_name ASC",:include=>[{:subjects=>:employees},:course], :page => params[:page], :per_page => 10)
 #    @batches = Batch.active :include=>[{:subjects=>:employees},:course]
     @subjects = @batches.collect(&:subjects).flatten
   end

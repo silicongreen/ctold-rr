ddf5f568a5dfb70a5b5b7f7158876c0f62a54f05
Fahim Mohammad Chowdhury <fahim.mohammad@teamworkbd.com>
2019-01-03 05:30:33 +0000

    some hanges tc

--- classtune/app/controllers/student_controller.rb
+++ classtune/app/controllers/student_controller.rb
@@ -1247,6 +1247,7 @@ class StudentController < ApplicationController
     
     student_electives = StudentsSubject.find_all_by_student_id(params[:id],:conditions=>"batch_id = #{@student.batch_id}")
     @group = ""
+    abort(student_electives.inspect)
     if student_electives
       student_electives.each do |elect|
        sub = Subject.find(elect.subject_id)

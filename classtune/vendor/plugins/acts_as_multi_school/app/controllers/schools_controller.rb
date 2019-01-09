206844ce06133c569380c02e38f2a6cec3a22d63
Fahim Mohammad Chowdhury <fahim.mohammad@teamworkbd.com>
2019-01-09 13:17:55 +0000

    summary report problem

--- classtune/vendor/plugins/acts_as_multi_school/app/controllers/schools_controller.rb
+++ classtune/vendor/plugins/acts_as_multi_school/app/controllers/schools_controller.rb
@@ -513,7 +513,7 @@ class SchoolsController <  MultiSchoolController
         
         unless @student.nil?
           #          @batch = Batch.find_by_id(@student[0].batch_id)
-          @batch = @batches.find{|b| b['id'] == @student.batch_id}
+          @batch = @batches.find{|d| d['id'] == @student.batch_id}
          
 #          Batch.find_by_sql ["SELECT * FROM batches WHERE id = ?", @student.batch_id]
           #@course = Course.find_by_id(@batch.course_id)

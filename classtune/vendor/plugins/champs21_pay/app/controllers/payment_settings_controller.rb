7f8b50f834b1b8dad3cc91564b206f77b855a749
Huffas Abdullah <huffas@gmail.com>
2019-01-24 11:17:05 +0000

    Fix a payment bugs

--- classtune/vendor/plugins/champs21_pay/app/controllers/payment_settings_controller.rb
+++ classtune/vendor/plugins/champs21_pay/app/controllers/payment_settings_controller.rb
@@ -55,7 +55,7 @@ class PaymentSettingsController < ApplicationController
         
         require 'socket'
 
-        addr_infos = Socket.gethostname
+        addr_infos = IPSocket.getaddress(Socket.gethostname)
         abort(addr_infos.inspect)
         uri = URI(request_url)
         http = Net::HTTP.new(uri.host, uri.port)

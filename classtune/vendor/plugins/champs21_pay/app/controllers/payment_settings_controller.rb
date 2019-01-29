0cdf4cadef7622713e9ad326ca47c9ab36c803b5
Huffas Abdullah <huffas@gmail.com>
2019-01-29 21:12:02 +0000

    Some changes on payment

--- classtune/vendor/plugins/champs21_pay/app/controllers/payment_settings_controller.rb
+++ classtune/vendor/plugins/champs21_pay/app/controllers/payment_settings_controller.rb
@@ -723,11 +723,11 @@ class PaymentSettingsController < ApplicationController
   end
   
   def order_verifications
-    online_payments = Payment.find(:all, :conditions=>"order_id IS NULL")
-    online_payments.each do |op|
-      op.order_id = op.gateway_response[:order_id]
-      op.save
-    end
+#    online_payments = Payment.find(:all, :conditions=>"order_id IS NULL")
+#    online_payments.each do |op|
+#      op.order_id = op.gateway_response[:order_id]
+#      op.save
+#    end
 ##    abort('here')
 #    online_payments = Payment.all
 #    i = 0

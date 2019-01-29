3cd993f39d5552e668c03b06c607b571f0e34a31
Huffas Abdullah <huffas@gmail.com>
2019-01-29 07:07:56 +0000

    Debug Order verification

--- classtune/vendor/plugins/champs21_pay/app/controllers/payment_settings_controller.rb
+++ classtune/vendor/plugins/champs21_pay/app/controllers/payment_settings_controller.rb
@@ -664,23 +664,24 @@ class PaymentSettingsController < ApplicationController
   end
   
   def order_verifications
-#    online_payments = Payment.all
-#    i = 0
-#    order_ids = []
-#    online_payments.each do |op|
-#      unless op.gateway_response[:verified].nil?
-#        verified = op.gateway_response[:verified]
-#        if verified.to_i == 0
-#          if op.gateway_response[:payment_type] != 'ITCL'
-#            order_ids[i] = op.gateway_response[:order_id]
-#            i += 1
-#            #if i > 100
-#            #  break
-#            #end
-#          end
-#        end
-#      end
-#    end
+    online_payments = Payment.all
+    i = 0
+    order_ids = []
+    online_payments.each do |op|
+      op.transaction_datetime = DateTime.parse(op.gateway_response[:tran_date])
+      unless op.gateway_response[:verified].nil?
+        verified = op.gateway_response[:verified]
+        if verified.to_i == 0
+          if op.gateway_response[:payment_type] != 'ITCL'
+            order_ids[i] = op.gateway_response[:order_id]
+            i += 1
+            #if i > 100
+            #  break
+            #end
+          end
+        end
+      end
+    end
 #    order_ids = ["410202", "588254", "889707", "346240", "284674", "752775", "900481", "144658", "994418", "805254", "145218", "487866", "126529", "977381", "352622", "180363", "871216", "180783", "510797", "913520", "989037", "191434", "782724", "350415", "923373", "669304", "242781"]
 #    abort(order_ids.inspect)
     
@@ -741,7 +742,6 @@ class PaymentSettingsController < ApplicationController
             end
 
             result = Base64.decode64(status)
-            abort(result.inspect)
             #s = Hash.from_xml(result).to_json
             #@financefee = FinanceFee.find(83278)
             #@student = Student.find(22845)

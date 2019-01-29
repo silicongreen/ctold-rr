cac1f902993024f998724596f42ed265174b2687
Huffas Abdullah <huffas@gmail.com>
2019-01-29 16:54:09 +0000

    Some changes on payment

--- classtune/vendor/plugins/champs21_pay/app/controllers/payment_settings_controller.rb
+++ classtune/vendor/plugins/champs21_pay/app/controllers/payment_settings_controller.rb
@@ -889,8 +889,6 @@ class PaymentSettingsController < ApplicationController
               if found_verified
                 childs = xml_transaction_infos[verifiedId].children
               else  
-                abort(result.to_s + "   " + o.to_s)
-                abort(xml_transaction_infos.inspect)
                 childs = xml_transaction_infos[xml_transaction_infos.length - 1].children
               end
 

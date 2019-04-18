9261056deaf3e056d41f01758c1f534b9012458f
Fahim Mohammad Chowdhury <fahim.mohammad@teamworkbd.com>
2019-04-18 09:22:08 +0000

    exam controller

--- classtune/app/controllers/exam_controller.rb
+++ classtune/app/controllers/exam_controller.rb
@@ -5240,7 +5240,7 @@ class ExamController < ApplicationController
               end 
 
               if batch_subject_id.include?(sub['id'].to_i) or std_subject_id.include?(sub['id'].to_i)
-                if fourth_subject == false && sub['subject_group_id'].to_i == 0
+                if fourth_subject == false && sub['subject_group_id'].to_i == 0 &&  sub['grade_subject'].to_i == 0
                   total_subject = total_subject+1
                 end
                 total_mark1 = 0
@@ -5384,7 +5384,7 @@ class ExamController < ApplicationController
                       if rs['quarter'] == '1'
                         total_ob1 = total_ob1+rs['result'][rs['exam_id']][sub['id']][std['id']]['marks_obtained'].to_f
                         if !rs['result'][rs['exam_id']][sub['id']][std['id']]['grade'].blank? && rs['result'][rs['exam_id']][sub['id']][std['id']]['grade'] == "F" && fourth_subject.blank? && (rs['result'][rs['exam_id']][sub['id']][std['id']]['marks_obtained'].to_i != 8 or rs['result'][rs['exam_id']][sub['id']][std['id']]['full_mark'].to_i != 25)
-                          if sub['subject_group_id'].to_i == 0 || sub['code'].to_s == "Ban-1" && (@connect_exam_obj.result_type != 3 or @connect_exam_obj.result_type != 4 or @connect_exam_obj.result_type != 7 or @connect_exam_obj.result_type != 8 or sub['grade_subject'].to_i != 1)
+                          unless sub['subject_group_id'].to_i > 0 or @connect_exam_obj.result_type == 3 or @connect_exam_obj.result_type == 4 or @connect_exam_obj.result_type == 7 or @connect_exam_obj.result_type == 8 or sub['grade_subject'].to_i == 1
                             u_grade1 = u_grade1+1
                             subject_failed = true
                           end
@@ -5393,7 +5393,7 @@ class ExamController < ApplicationController
                       if rs['quarter'] == '2'
                         total_ob2 = total_ob2+rs['result'][rs['exam_id']][sub['id']][std['id']]['marks_obtained'].to_f
                         if !rs['result'][rs['exam_id']][sub['id']][std['id']]['grade'].blank? && rs['result'][rs['exam_id']][sub['id']][std['id']]['grade'] == "F" && fourth_subject.blank? && (rs['result'][rs['exam_id']][sub['id']][std['id']]['marks_obtained'].to_i != 8 or rs['result'][rs['exam_id']][sub['id']][std['id']]['full_mark'].to_i != 25)
-                          if sub['subject_group_id'].to_i == 0 || sub['code'].to_s == "Ban-1" && (@connect_exam_obj.result_type != 3 or @connect_exam_obj.result_type != 4 or @connect_exam_obj.result_type != 7 or @connect_exam_obj.result_type != 8 or sub['grade_subject'].to_i != 1)
+                          unless sub['subject_group_id'].to_i > 0 or @connect_exam_obj.result_type == 3 or @connect_exam_obj.result_type == 4 or @connect_exam_obj.result_type == 7 or @connect_exam_obj.result_type == 8 or sub['grade_subject'].to_i == 1
                             u_grade2 = u_grade2+1
                             subject_failed = true
                           end

28a6d74b184b29fef4f896b7f4c1b067fe117b87
Fahim Mohammad Chowdhury <fahim.mohammad@teamworkbd.com>
2019-05-20 06:44:21 +0000

    oisd

--- classtune/app/controllers/exam_controller.rb
+++ classtune/app/controllers/exam_controller.rb
@@ -4578,6 +4578,17 @@ class ExamController < ApplicationController
         :bottom => 40,
         :left=> 10,
         :right => 10}
+    elsif MultiSchool.current_school.id == 355
+      render :pdf => template,
+          :save_to_file => file_name,
+          :save_only    => for_save,
+          :orientation => 'Portrait',
+          :margin => {    :top=> 10,
+          :bottom => 10,
+          :left=> 10,
+          :right => 10},
+          :header => {:html => { :template=> 'layouts/pdf_empty_header.html'}},
+          :footer => {:html => { :template=> 'layouts/pdf_empty_footer.html'}}
     elsif MultiSchool.current_school.id == 352 or MultiSchool.current_school.id == 346
       if @connect_exam_obj.result_type == 1 or @connect_exam_obj.result_type == 3 or @connect_exam_obj.result_type == 5 or @connect_exam_obj.result_type == 7 or @connect_exam_obj.result_type == 9 or @connect_exam_obj.result_type.to_i == 11
         render :pdf => template,

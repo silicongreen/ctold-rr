433b1f38116f428d8e2601ee311b63701522346f
Fahim Mohammad Chowdhury <fahim.mohammad@teamworkbd.com>
2019-06-19 08:54:03 +0000

    jpg

--- classtune/vendor/plugins/acts_as_multi_school/app/controllers/schools_controller.rb
+++ classtune/vendor/plugins/acts_as_multi_school/app/controllers/schools_controller.rb
@@ -485,16 +485,29 @@ class SchoolsController <  MultiSchoolController
     @total = params[:total]
     #@student_data = StudentsGuardians.find(:all,:conditions=>{:school_id=>@school.id})
     @conn = ActiveRecord::Base.connection 
-    if !@start_index.blank? and !@total.blank?
-      sql = "SELECT s.`id` as student_id,s.batch_id,s.`admission_no`  ,s.`first_name`,s.`middle_name`,s.`last_name`,s.`immediate_contact_id`,s.`school_id`,
-              fu.paid_username,fu.paid_password FROM 
-              students as s left join tds_free_users as fu on s.user_id=fu.paid_id where s.school_id=#{@school.id} and s.is_deleted = 0 order by s.id asc limit #{@total} OFFSET #{@start_index}"
-      
+    if MultiSchool.current_school.code = "sis"
+      if !@start_index.blank? and !@total.blank?
+        sql = "SELECT s.`id` as student_id,s.batch_id,s.`admission_no`  ,s.`first_name`,s.`middle_name`,s.`last_name`,s.`immediate_contact_id`,s.`school_id`,
+                fu.paid_username,fu.paid_password FROM 
+                students as s left join tds_free_users as fu on s.user_id=fu.paid_id where s.school_id=#{@school.id} and s.is_deleted = 0 order by s.admission_no asc limit #{@total} OFFSET #{@start_index}"
+
+      else
+        sql = "SELECT s.`id` as student_id,s.batch_id,s.`admission_no`  ,s.`first_name`,s.`middle_name`,s.`last_name`,s.`immediate_contact_id`,s.`school_id`,
+                fu.paid_username,fu.paid_password FROM 
+                students as s left join tds_free_users as fu on s.user_id=fu.paid_id where s.school_id=#{@school.id} and s.is_deleted = 0 order by s.admission_no"
+      end
     else
-      sql = "SELECT s.`id` as student_id,s.batch_id,s.`admission_no`  ,s.`first_name`,s.`middle_name`,s.`last_name`,s.`immediate_contact_id`,s.`school_id`,
-              fu.paid_username,fu.paid_password FROM 
-              students as s left join tds_free_users as fu on s.user_id=fu.paid_id where s.school_id=#{@school.id} and s.is_deleted = 0"
-    end  
+      if !@start_index.blank? and !@total.blank?
+        sql = "SELECT s.`id` as student_id,s.batch_id,s.`admission_no`  ,s.`first_name`,s.`middle_name`,s.`last_name`,s.`immediate_contact_id`,s.`school_id`,
+                fu.paid_username,fu.paid_password FROM 
+                students as s left join tds_free_users as fu on s.user_id=fu.paid_id where s.school_id=#{@school.id} and s.is_deleted = 0 order by s.id asc limit #{@total} OFFSET #{@start_index}"
+
+      else
+        sql = "SELECT s.`id` as student_id,s.batch_id,s.`admission_no`  ,s.`first_name`,s.`middle_name`,s.`last_name`,s.`immediate_contact_id`,s.`school_id`,
+                fu.paid_username,fu.paid_password FROM 
+                students as s left join tds_free_users as fu on s.user_id=fu.paid_id where s.school_id=#{@school.id} and s.is_deleted = 0"
+      end
+    end
     sql2 = "SELECT g.`first_name`,g.`last_name`,gs.student_id,g.office_phone1,g.mobile_phone,
   g.relation,fu.paid_username,fu.paid_password FROM 
   guardians as g left join tds_free_users as fu on g.user_id=fu.paid_id left join guardian_students as gs on g.id=gs.guardian_id where fu.paid_school_id=#{@school.id} and  g.school_id=#{@school.id}"
@@ -951,7 +964,7 @@ guardians as g left join tds_free_users as fu on g.user_id=fu.paid_id left join 
     @institutional_phone_no = ""
     @free_feed_for_admin = "0"
     @free_feed_for_teacher = "0"
-    @free_feed_for_student = "0"
+    @free_feed_for_student = "1"
     @is_test_school = 1    
     
     @school_group = admin_user_session.school_group

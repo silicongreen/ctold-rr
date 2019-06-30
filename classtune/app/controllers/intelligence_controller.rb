8545dff8e29a0dc4178f1fbba2df7cf8ea90afc0
Fahim Mohammad Chowdhury <fahim.mohammad@teamworkbd.com>
2019-06-30 07:28:11 +0000

    class int classwork

--- classtune/app/controllers/intelligence_controller.rb
+++ classtune/app/controllers/intelligence_controller.rb
@@ -287,16 +287,17 @@ class IntelligenceController < ApplicationController
       @sort_type = params[:student][:sort_type]
     end
     if !params[:start_date].blank?
-      @start_date = params[:start_date].to_date
+      @start_date = params[:start_date]
     end
     if !params[:select_date].blank?
-      @date = params[:select_date].to_date
+      @date = params[:select_date]
     end
     
     get_classwork_report_full_teacher(@department_id,@sort_by,@sort_type,@start_date,@date)
     @report_data = []
     if @student_response['status']['code'].to_i == 200
       @report_data = @student_response['data']
+      abort(@report_data.inspects)
     end
     render :pdf => "teacher_classwork_pdf",
     :orientation => 'Portrait',

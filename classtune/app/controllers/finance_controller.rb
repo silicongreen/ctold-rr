d911c680309be93636fab56aa867bde04a3a9687
Huffas Abdullah <huffas@gmail.com>
2019-07-13 11:12:13 +0000

    Debug a finance bug

--- classtune/app/controllers/finance_controller.rb
+++ classtune/app/controllers/finance_controller.rb
@@ -1410,7 +1410,7 @@ class FinanceController < ApplicationController
 
             @transactions = FinanceTransaction.find(:all, :conditions => ["payments.transaction_datetime >= '#{@start_date.to_date.strftime("%Y-%m-%d 00:00:00")}' and payments.transaction_datetime <= '#{@end_date.to_date.strftime("%Y-%m-%d 23:59:59")}' " + extra_params], :joins => "INNER JOIN payments ON finance_transactions.id = payments.finance_transaction_id " + extra_joins)
             #abort("payments.transaction_datetime >= '#{@start_date.to_date.strftime("%Y-%m-%d 00:00:00")}' and payments.transaction_datetime <= '#{@end_date.to_date.strftime("%Y-%m-%d 23:59:59")}'")
-            #@transactions = FinanceTransaction.find(:all, :joins => "INNER JOIN payments ON finance_transactions.id = payments.finance_transaction_id")
+            @transactions = FinanceTransaction.find(:all, :joins => "INNER JOIN payments ON finance_transactions.id = payments.finance_transaction_id")
             #abort(@transactions.length.to_s)
             @transactions.each do |pwt|
               if params[:test].to_i == 1

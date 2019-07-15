8bdff83253df7569ac73df544016d2de05255e52
Fahim Mohammad Chowdhury <fahim.mohammad@teamworkbd.com>
2019-07-15 11:24:43 +0000

    guardian student

--- src/api/protected/modules/api/controllers/DashboardController.php
+++ src/api/protected/modules/api/controllers/DashboardController.php
@@ -136,6 +136,7 @@ class DashboardController extends Controller
                 if($std_data)
                 {
                     $batch_id_new = $std_data->batch_id;
+                    Yii::app()->user->setState("batch_id", $batch_id_new);
                     //Yii::app()->session->set('user.batch_id',$batch_id_new);
                 }
             }

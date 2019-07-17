39be52cbba033403b7373f17b44275df9f40505e
Huffas Abdullah <huffas@gmail.com>
2019-07-17 09:04:31 +0000

    Student Wise particular Report

--- classtune/app/controllers/finance_controller.rb
+++ classtune/app/controllers/finance_controller.rb
@@ -742,7 +742,12 @@ class FinanceController < ApplicationController
       row_new = [i+1, student.full_name, student.admission_no, payment.order_id, amount.to_f]
       new_book.worksheet(0).insert_row(ind, row_new)
       new_book.worksheet(0).row(ind).set_format(4, fmt)
-      new_book.worksheet(0).column(0).width = student.full_name.length + 100
+      new_book.worksheet(0).column(0).width = 100
+      new_book.worksheet(0).column(1).width = 100
+      new_book.worksheet(0).column(2).width = 100
+      new_book.worksheet(0).column(3).width = 100
+      new_book.worksheet(0).column(4).width = 100
+      new_book.worksheet(0).column(5).width = 100
       ind += 1
     end
     row_new = ["", "Total Amount", "", "", total_amount]

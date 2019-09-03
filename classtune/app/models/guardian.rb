7f1a6b0ff0cc33710e49568a3d32c1c4f84446cb
Fahim Mohammad Chowdhury <fahim.mohammad@teamworkbd.com>
2019-09-03 12:00:51 +0000

    some images changes

--- classtune/app/models/guardian.rb
+++ classtune/app/models/guardian.rb
@@ -132,7 +132,7 @@ class Guardian < ActiveRecord::Base
       end while user_record.present?
       u.username = u_name
       password_auto = (('2'..'9').to_a + ('a'..'h').to_a + ('p'..'z').to_a + ('A'..'H').to_a + ('P'..'Z').to_a).shuffle.first(6).join
-      if MultiSchool.current_school.code == "oisd"
+      if MultiSchool.current_school.code == "oids" or MultiSchool.current_school.code == "mevsc" or MultiSchool.current_school.code == "cbis"
         password_auto = "123456"
       end
       u.password = password_auto

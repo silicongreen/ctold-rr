c9047b29048fc1765b995a18281e26cdfb778f48
Fahim Mohammad Chowdhury <fahim.mohammad@teamworkbd.com>
2019-09-04 11:03:01 +0000

    changes in south point

--- classtune/app/controllers/marks_controller.rb
+++ classtune/app/controllers/marks_controller.rb
@@ -136,7 +136,7 @@ class MarksController < ApplicationController
     elsif data_type.to_i == 1
       @exam_connect = ExamConnect.active.find(:all,:conditions=>"batch_id="+batch_id+" and (result_type=3 or result_type=4) and is_deleted=0")
     elsif data_type.to_i == 2
-      @exam_connect = ExamConnect.active.find(:all,:conditions=>"batch_id="+batch_id+" and (result_type=1 or result_type=2 or result_type=5  or result_type=6  or result_type=7) and is_deleted=0")
+      @exam_connect = ExamConnect.active.find(:all,:conditions=>"batch_id="+batch_id+" and (result_type=1 or result_type=2 or result_type=5  or result_type=6  or result_type=7  or result_type=8) and is_deleted=0")
     else
       @exam_connect = ExamConnect.active.find(:all,:conditions=>"batch_id="+batch_id+" and is_deleted=0")
     end 

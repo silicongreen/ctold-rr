a3d43e1667b5095720ac28f99027df04b8fb0e8f
ahmabdullah <huffas@gmail.com>
2019-09-26 16:42:18 +0600

    unstage changes

--- src/api/1.php
+++ src/api/1.php
@@ -1,2 +1,13 @@
 <?php
-echo phpinfo();
+
+print_r($_POST);
+  
+  
+ ?>
+<form method="post"  enctype="multipart/form-data" >
+    <textarea name="test">
+        
+    </textarea>
+    
+    <input type="submit" value="submit" name="submit" />
+</form>

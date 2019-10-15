e26e5c6a90ab2b7b1f6916b81dbbbb2e4fba3519
Fahim Mohammad Chowdhury <fahim.mohammad@teamworkbd.com>
2019-10-15 09:52:40 +0000

    exam changes

--- classtune/app/controllers/exam_controller.rb
+++ classtune/app/controllers/exam_controller.rb
@@ -4987,7 +4987,7 @@ class ExamController < ApplicationController
         :left=> 10,
         :right => 10},
         :header => {:html => { :template=> 'layouts/pdf_empty_header.html'}},
-        :footer => {:html => { :template=> 'layouts/pdf_footer_baghc.html'}}
+        :footer => {:html => { :template=> 'layouts/pdf_empty_footer.html'}}
       end  
     elsif  MultiSchool.current_school.id == 312 or MultiSchool.current_school.id == 2 
       if @connect_exam_obj.result_type != 1 and @connect_exam_obj.result_type != 6 and @connect_exam_obj.result_type != 7

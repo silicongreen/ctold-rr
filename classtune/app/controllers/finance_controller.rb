d885b6cc434afe75e990654c53754119ad01052b
Huffas Abdullah <huffas@gmail.com>
2019-11-28 07:57:22 +0000

    Order ID already paid debug

--- classtune/app/controllers/finance_controller.rb
+++ classtune/app/controllers/finance_controller.rb
@@ -10139,7 +10139,7 @@ class FinanceController < ApplicationController
             s = []
             i = 0
             tot_amount = 0.00
-    @transactions = FinanceTransaction.find(:all, :conditions => ["finance_transactions.fine_included = 1 and payments.transaction_datetime >= '#{@start_date.to_date.strftime("%Y-%m-%d 00:00:00")}' and payments.transaction_datetime <= '#{@end_date.to_date.strftime("%Y-%m-%d 23:59:59")}' "], :joins => "INNER JOIN payments ON finance_transactions.id = payments.finance_transaction_id ")
+    @transactions = FinanceTransaction.find(:all, :conditions => ["payments.transaction_datetime >= '#{@start_date.to_date.strftime("%Y-%m-%d 00:00:00")}' and payments.transaction_datetime <= '#{@end_date.to_date.strftime("%Y-%m-%d 23:59:59")}' "], :joins => "INNER JOIN payments ON finance_transactions.id = payments.finance_transaction_id ")
     #abort(@transactions.inspect)
             #abort("payments.transaction_datetime >= '#{@start_date.to_date.strftime("%Y-%m-%d 00:00:00")}' and payments.transaction_datetime <= '#{@end_date.to_date.strftime("%Y-%m-%d 23:59:59")}'")
             #@transactions = FinanceTransaction.find(:all, :joins => "INNER JOIN payments ON finance_transactions.id = payments.finance_transaction_id")

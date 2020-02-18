1bbd974af8a39d93ee8ed96bd55179f55fd29f92
Huffas Abdullah <huffas@gmail.com>
2020-02-18 08:01:19 +0000

    Some Changes on Auto Advance Adjustment

--- classtune/vendor/plugins/acts_as_multi_school/lib/finance_loader.rb
+++ classtune/vendor/plugins/acts_as_multi_school/lib/finance_loader.rb
@@ -3201,4 +3201,388 @@ module FinanceLoader
     verify_order
   end
   
+  def validate_payment_types(params)
+    if params[:create_cancel_transaction].present?
+        gateway_response = {
+          :amount => params[:amount],
+          :status => params[:status],
+          :tran_id=>params[:tran_id],
+          :tran_date=>params[:tran_date]
+
+        }
+        payment = Payment.new(:payee => @student,:payment => @financefee,:gateway_response => gateway_response)
+        payment.save
+        flash[:notice] = "#{t('payment_canceled')}"
+    elsif params[:create_fail_transaction].present?
+        gateway_response = {
+          :amount => params[:amount],
+          :status => params[:status],
+          :transaction_id => params[:bank_tran_id],
+          :tran_id=>params[:tran_id],
+          :tran_date=>params[:tran_date],
+          :card_type=>params[:card_type],
+          :card_no=>params[:card_no],
+          :card_issuer=>params[:card_issuer],
+          :card_brand=>params[:card_brand],
+          :card_issuer_country=>params[:card_issuer_country],
+          :card_issuer_country_code=>params[:card_issuer_country_code],
+          :error => params[:error]
+        }
+        payment = Payment.new(:payee => @student,:payment => @financefee,:gateway_response => gateway_response)
+        payment.save
+
+        flash[:notice] = "#{t('payment_failed')} : #{params[:error]}"      
+    elsif params[:create_transaction].present?
+      gateway_response = Hash.new
+      if @active_gateway == "Paypal"
+          gateway_response = {
+            :amount => params[:amt],
+            :status => params[:st],
+            :transaction_id => params[:tx]
+          }
+      elsif @active_gateway == "Authorize.net"
+          gateway_response = {
+            :x_response_code => params[:x_response_code],
+            :x_response_reason_code => params[:x_response_reason_code],
+            :x_response_reason_text => params[:x_response_reason_text],
+            :x_avs_code => params[:x_avs_code],
+            :x_auth_code => params[:x_auth_code],
+            :x_trans_id => params[:x_trans_id],
+            :x_method => params[:x_method],
+            :x_card_type => params[:x_card_type],
+            :x_account_number => params[:x_account_number],
+            :x_first_name => params[:x_first_name],
+            :x_last_name => params[:x_last_name],
+            :x_company => params[:x_company],
+            :x_address => params[:x_address],
+            :x_city => params[:x_city],
+            :x_state => params[:x_state],
+            :x_zip => params[:x_zip],
+            :x_country => params[:x_country],
+            :x_phone => params[:x_phone],
+            :x_fax => params[:x_fax],
+            :x_invoice_num => params[:x_invoice_num],
+            :x_description => params[:x_description],
+            :x_type => params[:x_type],
+            :x_cust_id => params[:x_cust_id],
+            :x_ship_to_first_name => params[:x_ship_to_first_name],
+            :x_ship_to_last_name => params[:x_ship_to_last_name],
+            :x_ship_to_company => params[:x_ship_to_company],
+            :x_ship_to_address => params[:x_ship_to_address],
+            :x_ship_to_city => params[:x_ship_to_city],
+            :x_ship_to_zip => params[:x_ship_to_zip],
+            :x_ship_to_country => params[:x_ship_to_country],
+            :x_amount => params[:x_amount],
+            :x_tax => params[:x_tax],
+            :x_duty => params[:x_duty],
+            :x_freight => params[:x_freight],
+            :x_tax_exempt => params[:x_tax_exempt],
+            :x_po_num => params[:x_po_num],
+            :x_cvv2_resp_code => params[:x_cvv2_resp_code],
+            :x_MD5_hash => params[:x_MD5_hash],
+            :x_cavv_response => params[:x_cavv_response],
+            :x_method_available => params[:x_method_available],
+          }    
+      elsif @active_gateway == "ssl.commerce"
+          gateway_response = {
+            :amount => params[:amount],
+            :status => params[:status],
+            :transaction_id => params[:bank_tran_id],
+            :tran_id=>params[:tran_id],
+            :tran_date=>params[:tran_date],
+            :card_type=>params[:card_type],
+            :card_no=>params[:card_no],
+
+            :card_issuer=>params[:card_issuer],
+
+            :card_brand=>params[:card_brand],
+
+            :card_issuer_country=>params[:card_issuer_country],
+
+            :card_issuer_country_code=>params[:card_issuer_country_code],
+            :val_id => params[:val_id]
+          }    
+      elsif @active_gateway == "trustbank"
+        result = Base64.decode64(params[:CheckoutXmlMsg])
+        #result = '<Response date="2016-06-20 10:14:53.213">  <RefID>133783A000129D</RefID>  <OrderID>O100010</OrderID>  <Name> Customer1</Name>  <Email> mr.customer@gmail.com </Email>  <Amount>2090.00</Amount>  <ServiceCharge>0.00</ServiceCharge>  <Status>1</Status>  <StatusText>PAID</StatusText>  <Used>0</Used>  <Verified>0</Verified>  <PaymentType>ITCL</PaymentType>  <PAN>712300XXXX1277</PAN>  <TBMM_Account></TBMM_Account>  <MarchentID>SAGC</MarchentID>  <OrderDateTime>2016-06-20 10:14:24.700</OrderDateTime>  <PaymentDateTime>2016-06-20 10:21:34.303</PaymentDateTime>  <EMI_No>0</EMI_No>  <InterestAmount>0.00</InterestAmount>  <PayWithCharge>1</PayWithCharge>  <CardResponseCode>00</CardResponseCode>  <CardResponseDescription>APPROVED</CardResponseDescription>  <CardOrderStatus>APPROVED</CardOrderStatus> </Response> '
+        xml_res = Nokogiri::XML(result)
+        status_post = 0
+        status_text_post = ""
+        amount_post = 0.00
+        service_charge_post = 0.00
+        trans_date = 0.00
+        payment_type = ""
+        pan = ""
+        ref_id = ""
+        used = ""
+        verified = ""
+        name = ""
+        email = ""
+        tbbmm_account = ""
+        merchant_id = ""
+        order_datetime = ""
+        emi_no = ""
+        interest_amount = ""
+        pay_with_charge = ""
+        card_response_code = ""
+        card_response_desc = ""
+        card_order_status = ""
+        unless xml_res.xpath("//Response/Name").empty?
+          name = xml_res.xpath("//Response/Name").text
+        end
+        unless xml_res.xpath("//Response/Email").empty?
+          email = xml_res.xpath("//Response/Email").text
+        end
+        unless xml_res.xpath("//Response/MarchentID").empty?
+          merchant_id = xml_res.xpath("//Response/MarchentID").text
+        end
+        unless xml_res.xpath("//Response/OrderDateTime").empty?
+          order_datetime = xml_res.xpath("//Response/OrderDateTime").text
+        end
+        unless xml_res.xpath("//Response/EMI_No").empty?
+          emi_no = xml_res.xpath("//Response/EMI_No").text
+        end
+        unless xml_res.xpath("//Response/TBMM_Account").empty?
+          tbbmm_account = xml_res.xpath("//Response/TBMM_Account").text
+        end
+        unless xml_res.xpath("//Response/InterestAmount").empty?
+          interest_amount = xml_res.xpath("//Response/InterestAmount").text
+        end
+        unless xml_res.xpath("//Response/PayWithCharge").empty?
+          pay_with_charge = xml_res.xpath("//Response/PayWithCharge").text
+        end
+        unless xml_res.xpath("//Response/CardResponseCode").empty?
+          card_response_code = xml_res.xpath("//Response/CardResponseCode").text
+        end
+        unless xml_res.xpath("//Response/CardResponseDescription").empty?
+          card_response_desc = xml_res.xpath("//Response/CardResponseDescription").text
+        end
+        unless xml_res.xpath("//Response/CardOrderStatus").empty?
+          card_order_status = xml_res.xpath("//Response/CardOrderStatus").text
+        end
+        unless xml_res.xpath("//Response/Status").empty?
+          status_post = xml_res.xpath("//Response/Status").text
+        end
+        unless xml_res.xpath("//Response/StatusText").empty?
+          status_text_post = xml_res.xpath("//Response/StatusText").text
+        end
+        unless xml_res.xpath("//Response/Used").empty?
+          used = xml_res.xpath("//Response/Used").text
+        end
+        unless xml_res.xpath("//Response/Verified").empty?
+          verified = xml_res.xpath("//Response/Verified").text
+        end
+        unless xml_res.xpath("//Response/Amount").empty?
+          amount_post = xml_res.xpath("//Response/Amount").text
+        end
+        unless xml_res.xpath("//Response/ServiceCharge").empty?
+          service_charge_post = xml_res.xpath("//Response/ServiceCharge").text
+        end
+        unless xml_res.xpath("//Response/OrderID").empty?
+          orderId = xml_res.xpath("//Response/OrderID").text
+        end
+        unless xml_res.xpath("//Response/RefID").empty?
+          ref_id = xml_res.xpath("//Response/RefID").text
+        end
+        unless xml_res.xpath("//Response/PaymentDateTime").empty?
+          trans_date = xml_res.xpath("//Response/PaymentDateTime").text
+        end
+        unless xml_res.xpath("//Response/PaymentType").empty?
+          payment_type = xml_res.xpath("//Response/PaymentType").text
+        end
+        unless xml_res.xpath("//Response/PAN").empty?
+          pan = xml_res.xpath("//Response/PAN").text
+        end
+
+        gateway_response = {
+          :amount => amount_post,
+          :name => name,
+          :email => email,
+          :merchant_id => merchant_id,
+          :order_datetime => order_datetime,
+          :emi_no => emi_no,
+          :tbbmm_account => tbbmm_account,
+          :interest_amount => interest_amount,
+          :pay_with_charge => pay_with_charge,
+          :card_response_code => card_response_code,
+          :card_response_desc => card_response_desc,
+          :card_order_status => card_order_status,
+          :used => used,
+          :verified => verified,
+          :status_text => status_text_post,
+          :status => status_post,
+          :ref_id => ref_id,
+          :order_id=>orderId,
+          :tran_date=>trans_date,
+          :payment_type=>payment_type,
+          :service_charge=>service_charge_post,
+          :pan=>pan
+        }
+        return_order_id = orderId.strip
+
+        @finance_order = FinanceOrder.find_by_order_id(orderId.strip)
+        #abort(@finance_order.inspect)
+        request_params = @finance_order.request_params
+
+        dt = trans_date.split(".")
+        transaction_datetime = dt[0]
+
+        if verified.to_i == 0
+          if transaction_datetime.nil?
+            dt = order_datetime.split(".")
+            transaction_datetime = dt[0]
+          end
+        end
+
+      end
+      
+      payment_saved = false
+      unless request_params.nil?
+        multiple = request_params[:multiple]
+        unless multiple.nil?
+          if multiple.to_s == "true"
+            fees = request_params[:fees].split(",")
+            fees.each do |fee|
+              f = fee.to_i
+              feenew = FinanceFee.find(f)
+              payment = Payment.find(:first, :conditions => "order_id = '#{orderId}' and payee_id = #{@student.id} and payment_id = #{feenew.id}")
+              unless payment.nil?
+                payment_saved = true
+              else  
+                payment = Payment.new(:order_id => orderId, :payee => @student,:payment => feenew,:gateway_response => gateway_response, :transaction_datetime => transaction_datetime)
+                if payment.save
+                  payment_saved = true
+                end 
+              end
+            end
+          else
+            payment = Payment.find(:first, :conditions => "order_id = '#{orderId}' and payee_id = #{@student.id} and payment_id = #{@financefee.id}")
+            unless payment.nil?
+              payment_saved = true
+            else  
+              payment = Payment.new(:order_id => orderId, :payee => @student,:payment => @financefee,:gateway_response => gateway_response, :transaction_datetime => transaction_datetime)
+              if payment.save
+                payment_saved = true
+              end 
+            end
+          end
+        else
+          payment = Payment.find(:first, :conditions => "order_id = '#{orderId}' and payee_id = #{@student.id} and payment_id = #{@financefee.id}")
+          unless payment.nil?
+            payment_saved = true
+          else  
+            payment = Payment.new(:order_id => orderId, :payee => @student,:payment => @financefee,:gateway_response => gateway_response, :transaction_datetime => transaction_datetime)
+            if payment.save
+              payment_saved = true
+            end 
+          end
+        end
+      end
+      
+      if payment_saved
+        gateway_status = false
+        if @active_gateway == "Paypal"
+          gateway_status = true if params[:st] == "Completed"
+        elsif @active_gateway == "Authorize.net"
+          gateway_status = true if params[:x_response_reason_code] == "1"
+        elsif @active_gateway == "ssl.commerce"
+          gateway_status = true if params[:status] == "VALID"
+          if gateway_status == true
+            testssl = false
+            if PaymentConfiguration.config_value('is_test_sslcommerz').to_i == 1
+              if File.exists?("#{Rails.root}/vendor/plugins/champs21_pay/config/payment_config.yml")
+                payment_configs = YAML.load_file(File.join(Rails.root,"vendor/plugins/champs21_pay/config/","payment_config.yml"))
+                unless payment_configs.nil? or payment_configs.empty? or payment_configs.blank?
+                  testssl = payment_configs["testsslcommer"]
+                end
+              end
+              if testssl 
+                validation_url = payment_configs["validation_api"]
+              else
+                payment_urls = Hash.new
+                if File.exists?("#{Rails.root}/vendor/plugins/champs21_pay/config/online_payment_url.yml")
+                  payment_urls = YAML.load_file(File.join(Rails.root,"vendor/plugins/champs21_pay/config/","online_payment_url.yml"))
+                end
+                if MultiSchool.current_school.id == 2
+                  validation_url = payment_urls["ssl_commerce_sandbox_requested_url"]
+                  validation_url ||= "https://sandbox.sslcommerz.com/validator/api/validationserverAPI.php"
+                else
+                  validation_url = payment_urls["ssl_commerce_requested_url"]
+                  validation_url ||= "https://securepay.sslcommerz.com/validator/api/validationserverAPI.php"
+                end
+              end
+            else
+              payment_urls = Hash.new
+              if File.exists?("#{Rails.root}/vendor/plugins/champs21_pay/config/online_payment_url.yml")
+                payment_urls = YAML.load_file(File.join(Rails.root,"vendor/plugins/champs21_pay/config/","online_payment_url.yml"))
+              end
+              if MultiSchool.current_school.id == 2
+                validation_url = payment_urls["ssl_commerce_sandbox_requested_url"]
+                validation_url ||= "https://sandbox.sslcommerz.com/validator/api/validationserverAPI.php"
+              else
+                validation_url = payment_urls["ssl_commerce_requested_url"]
+                validation_url ||= "https://securepay.sslcommerz.com/validator/api/validationserverAPI.php"
+              end
+            end
+
+            val_id = params[:val_id]
+            requested_url=validation_url+"?val_id="+val_id+"&store_id="+@store_id+"&store_passwd="+@store_password  
+            uri = URI.parse(requested_url)
+
+
+            http = Net::HTTP.new(uri.host, uri.port)
+            http.use_ssl = true
+
+            request = Net::HTTP::Post.new(uri.request_uri)
+            response = http.request(request)
+
+            @ssl_data = JSON::parse(response.body)
+            unless @ssl_data['status'] == "VALID"
+              gateway_status = false
+            end
+          end
+        elsif @active_gateway == "trustbank"
+          gateway_status = true if status_post.to_i == 1
+
+          if gateway_status == true
+            unless order_verify_trust_bank(orderId)
+              msg = "Payment unsuccessful!! Invalid Transaction, Amount or service charge mismatch"
+              gateway_status = false
+            else
+              gateway_status = true
+            end
+          end
+        end
+
+        amount_from_gateway = 0
+        if @active_gateway == "Paypal"
+          amount_from_gateway = params[:amt]
+        elsif @active_gateway == "Authorize.net"
+          amount_from_gateway = params[:x_amount]
+        elsif @active_gateway == "ssl.commerce"
+          amount_from_gateway=params[:amount]
+        elsif @active_gateway == "trustbank"
+          amount_from_gateway=amount
+        end
+
+        #trans_id=@financefee.fee_transactions.collect(&:finance_transaction_id).join(",")
+
+        unless request_params.nil?
+          total_fees = request_params[:amount_to_pay]
+        end
+
+        if gateway_status == false
+          if @active_gateway == "trustbank"
+            flash[:notice] = msg
+          else
+            flash[:notice] = "#{t('payment_failed')}"
+          end
+        end 
+      else
+        flash[:notice] = "#{t('payment_failed')}"
+      end 
+    end
+    
+  end
+  
 end

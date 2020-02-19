32b473418ea0ecd42f80e1735f2a7ed9e0dab74c
Huffas Abdullah <huffas@gmail.com>
2020-02-19 13:33:02 +0000

    Some Changes on Auto Advance Adjustment

--- classtune/vendor/plugins/acts_as_multi_school/lib/finance_loader.rb
+++ classtune/vendor/plugins/acts_as_multi_school/lib/finance_loader.rb
@@ -3207,7 +3207,7 @@ module FinanceLoader
                   total_fees = fee.balance.to_f+@fine_amount.to_f
 
                   amount_from_gateway = amount
-abort(amount_from_gateway.inspect)
+#abort(amount_from_gateway.inspect)
                   #abort(amount_from_gateway.to_s + " " + total_fees.to_s + "  " + @fine_amount.to_s)
                   pay_student(amount_from_gateway, total_fees, request_params, finance_order.order_id, verification_trans_date, ref_id)
                 end

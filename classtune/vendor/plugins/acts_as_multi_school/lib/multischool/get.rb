d2e5c86981253c04e8a3cfafd716d03acecc2e5f
Huffas Abdullah <huffas@gmail.com>
2020-03-03 14:15:07 +0000

    Bkash payment done from web and mobile

--- classtune/vendor/plugins/acts_as_multi_school/lib/multischool/get.rb
+++ classtune/vendor/plugins/acts_as_multi_school/lib/multischool/get.rb
@@ -240,7 +240,7 @@ module MultiSchool
             self.table_name = MultiSchool.current_school.code + "_finance_orders"
           else
             self.table_name = "finance_orders"  
-          end
+          end 
         end 
         with_scope(:find => {:conditions  => {:school_id  => target_school.id}}) do
           super

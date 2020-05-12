137e9b2371ba3e278a6e85967dad1f8a9ddf57f8
Huffas Abdullah <huffas@gmail.com>
2020-05-12 16:59:32 +0000

    City Bank CRT and CSR

--- classtune/app/controllers/student_controller.rb
+++ classtune/app/controllers/student_controller.rb
@@ -724,7 +724,7 @@ class StudentController < ApplicationController
         keyCAData = File.read(keyCA)
         is_test_citybank = PaymentConfiguration.config_value("is_test_citybank")
         extra_string = (is_test_citybank) ? '_sandbox' : ''
-        #abort(rootCAData.inspect)
+        abort(rootCAData.inspect)
         payment_url = URI(payment_urls["citybank_app_url" + extra_string] + "token")
         payment_url ||= URI("https://sandbox.thecitybank.com:7788/transaction/token")
         #abort(payment_url.inspect)

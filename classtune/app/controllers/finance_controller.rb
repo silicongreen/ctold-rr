ee86f1d57c88ac6cb3de92f77586d7fe73ebc0a8
Huffas Abdullah <huffas@gmail.com>
2020-05-16 23:13:37 +0000

    Details on History tab: Student Fees page

--- classtune/app/controllers/finance_controller.rb
+++ classtune/app/controllers/finance_controller.rb
@@ -85,7 +85,7 @@ class FinanceController < ApplicationController
         @valid_fee_transactions.each do |valid_fee_transaction|
           student_fee_ledgers = StudentFeeLedger.find(:first, :select => "student_id, sum( amount_to_pay ) - sum( amount_paid ) as diff_amount", :conditions => ["student_id = #{valid_fee_transaction.payee_id} "])
           unless student_fee_ledgers.blank?
-            if student_fee_ledgers.diff_amoun.to_f >= 0
+            if student_fee_ledgers.diff_amount.to_f == 0
               error_order << valid_fee_transaction.id
             end
           end

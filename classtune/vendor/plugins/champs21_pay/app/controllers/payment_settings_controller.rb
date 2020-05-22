7ef929f9de9e1970d670022b7efd38fdbd854a18
Huffas Abdullah <huffas@gmail.com>
2020-05-22 19:03:14 +0000

    Citybank Failed or Cancel Transaction

--- classtune/vendor/plugins/champs21_pay/app/controllers/payment_settings_controller.rb
+++ classtune/vendor/plugins/champs21_pay/app/controllers/payment_settings_controller.rb
@@ -91,7 +91,7 @@ class PaymentSettingsController < ApplicationController
 
       extra_query = ""
       if @gateway == "citybank"
-        extra_query += ' and gateway_response like \'%:Message:%\''
+        extra_query += ' and gateway_response like \'%Message:%\''
       end
       unless params[:payment_status].nil? or params[:payment_status].empty? or params[:payment_status].blank?
         payment_status = 0
@@ -181,7 +181,7 @@ class PaymentSettingsController < ApplicationController
 
             extra_query = ""
             if @gateway == "citybank"
-              extra_query += ' and gateway_response like \'%:Message:%\''
+              extra_query += ' and gateway_response like \'%Message:%\''
             end
             
             unless params[:payment_status].nil? or params[:payment_status].empty? or params[:payment_status].blank?

d1a3ef2c273c64d864d3cc605b4c877939355878
Huffas Abdullah <huffas@gmail.com>
2020-06-11 15:43:29 +0000

    Citybank Failed or Cancel Transaction

--- classtune/vendor/plugins/champs21_pay/app/controllers/payment_settings_controller.rb
+++ classtune/vendor/plugins/champs21_pay/app/controllers/payment_settings_controller.rb
@@ -596,7 +596,7 @@ class PaymentSettingsController < ApplicationController
                                   payments.each do |payment|
                                     paymentID = payment.gateway_response[:paymentID]
                                     query_info = query_bkash_payment(tokens[:id_token], paymentID)  
-                                    unless [6728,6731].include?(payment.id)
+                                    if payment.id == 6832
                                     abort(query_info.inspect)
                                     end
                                     unless query_info[:trxID].blank?

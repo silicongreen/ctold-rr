8a2d9605f204dd2a751f48064887999cb75801e1
Huffas Abdullah <huffas@gmail.com>
2020-06-11 20:42:19 +0000

    Citybank Failed or Cancel Transaction

--- classtune/vendor/plugins/acts_as_multi_school/lib/finance_loader.rb
+++ classtune/vendor/plugins/acts_as_multi_school/lib/finance_loader.rb
@@ -3854,6 +3854,7 @@ module FinanceLoader
           #abort(payment_saved.inspect)
           if payment_saved
             bkash_finance_orders = FinanceOrder.find(:all, :conditions => "order_id = '#{orderId}' and student_id = '#{@student.id}'")
+            abort(bkash_finance_orders.inspect)
             unless bkash_finance_orders.blank?
               total_fees = 0.00
               bkash_finance_orders.each do |finance_order|
@@ -3875,7 +3876,7 @@ module FinanceLoader
             end
             fee_percent = 0.00
             fee_percent = total_fees.to_f * (1.5 / 100)
-            abort(total_fees.to_s)
+            #abort(total_fees.to_s)
             amount = response_ssl[:amount]
             if (amount.to_f + fee_percent.to_f) == total_fees
               amount = amount.to_f - fee_percent.to_f

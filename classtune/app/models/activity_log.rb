592d006aa39d667efcb1749e9098d3374a451995
Fahim Mohammad Chowdhury <fahim.cse@gmail.com>
2020-06-23 13:28:49 +0600

    activity log

--- classtune/app/models/activity_log.rb
+++ classtune/app/models/activity_log.rb
@@ -5,5 +5,11 @@ class ActivityLog < ActiveRecord::Base
   def before_save
     self.school_id = MultiSchool.current_school.id
   end
+  
+  after_initialize do |activity_log|
+    unless MultiSchool.current_school.nil?
+        self.table_name = MultiSchool.current_school.code + "_activity_logs"
+    end
+  end
  
 end

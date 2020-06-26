d0d32aa1d016e7cd8496ffd5e3bf356176b8a9b0
Fahim Mohammad Chowdhury <fahim.cse@gmail.com>
2020-06-26 15:41:32 +0600

    online exam history

--- classtune/vendor/plugins/champs21_online_exam/app/models/online_exam_attendance.rb
+++ classtune/vendor/plugins/champs21_online_exam/app/models/online_exam_attendance.rb
@@ -1,10 +1,12 @@
 class OnlineExamAttendance < ActiveRecord::Base
   has_many :online_exam_score_details, :dependent => :destroy
+  has_many :online_exam_score_history_details, :dependent => :destroy
   belongs_to :online_exam_group
   belongs_to :student
 
   validates_associated :online_exam_score_details
   accepts_nested_attributes_for :online_exam_score_details ,:reject_if=>:reject_exam_score , :allow_destroy=>true
+  accepts_nested_attributes_for :online_exam_score_history_details
 
   def reject_exam_score(attributed)
     exam_option_id=attributed["online_exam_option_id"].to_i

bc1b11651ecc14045ea3da7d76a0d0deb138ab8e
Fahim Mohammad Chowdhury <fahim.cse@gmail.com>
2020-07-07 17:54:29 +0600

    css change

--- classtune/app/controllers/exam_controller.rb
+++ classtune/app/controllers/exam_controller.rb
@@ -6242,7 +6242,18 @@ class ExamController < ApplicationController
           :right => 10},
           :header => {:html => { :template=> 'layouts/pdf_empty_header.html'}},
           :footer => {:html => { :template=> 'layouts/pdf_empty_footer.html'}}
-      else  
+      elsif MultiSchool.current_school.id == 323 and @connect_exam_obj.result_type == 9
+          render :pdf => template,
+          :save_to_file => file_name,
+          :save_only    => for_save,
+          :orientation => 'Portrait',
+          :margin => {    :top=> 10,
+          :bottom => 10,
+          :left=> 10,
+          :right => 10},
+          :header => {:html => { :template=> 'layouts/pdf_empty_header.html'}},
+          :footer => {:html => { :template=> 'layouts/pdf_empty_footer.html'}}
+      else
         render :pdf => template,
           :save_to_file => file_name,
           :save_only    => for_save,

4a0e16abc94ce865e454cbf9379d0d76db518bcb
Huffas Abdullah <huffas@gmail.com>
2020-07-25 15:18:35 +0000

    Fees import modification

--- classtune/app/controllers/finance_controller.rb
+++ classtune/app/controllers/finance_controller.rb
@@ -17330,14 +17330,12 @@ class FinanceController < ApplicationController
         receiver_id = @fee_discount.receiver_id
         if @fee_discount.receiver_type == "Student"
           @fee_discounts = FeeDiscount.find(:all, :conditions => "scholarship_id = #{scholarship_id} and receiver_id = #{receiver_id} and is_deleted = #{false}")
-          abort(@fee_discounts.inspect)
+          
           unless @fee_discounts.blank?
             @fee_discounts.each do |fee_dis|
-              fee_discounts_paid_single = FinanceTransactionParticular.find(:all, :conditions => "particular_type = 'Adjustment' AND transaction_type = 'Discount' AND particular_id = " + fee_dis.id.to_s + "")
-              unless fee_discounts_paid_single.blank?
                 f_dis = FeeDiscount.find(fee_dis.id)
+                abort(f_dis.inspect)
                 f_dis.update_attributes(:is_deleted=>true)
-              end
             end
           end
         end

ae9298491429469a23c7e62ddf99c046634d1f32
Huffas Abdullah <huffas@gmail.com>
2020-08-16 15:34:17 +0000

    Fees import modification

--- classtune/vendor/plugins/champs21_pay/lib/online_payment/student_pay.rb
+++ classtune/vendor/plugins/champs21_pay/lib/online_payment/student_pay.rb
@@ -81,7 +81,8 @@ module OnlinePayment
           #@fee_particulars = @date.finance_fee_particulars.all(:conditions=>"is_deleted=#{false} and batch_id=#{@financefee.batch_id}").select{|par|  (par.receiver.present?) and (par.receiver==@student or par.receiver==@student.student_category or par.receiver==@financefee.batch) }
           #@total_payable=@fee_particulars.map{|s| s.amount}.sum.to_f
           
-          if request.post? and params[:order_id].present?
+          if request.post? 
+             ## and params[:order_id].present?
             
             @fee_collection_name = ( params[:fee_collection_name].blank? ) ? "Student Fees" : params[:fee_collection_name]
             @user_gateway = @gateway_settings.keys[0].to_s
@@ -238,17 +239,19 @@ module OnlinePayment
                 fee_requests = params[:id2]
               end
             end
-            now = I18n.l(@local_tzone_time.to_datetime, :format=>'%Y-%m-%d %H:%M:%S')
-            activity_log = ActivityLog.new
-            activity_log.user_id = current_user.id
-            activity_log.controller = "Finance Log - POST CHECK"
-            activity_log.action = params[:order_id].to_s
-            activity_log.post_requests = params
-            activity_log.ip = request.remote_ip
-            activity_log.user_agent = request.user_agent
-            activity_log.created_at = now
-            activity_log.updated_at = now
-            activity_log.save
+            if params[:CheckoutXmlMsg].present?
+              now = I18n.l(@local_tzone_time.to_datetime, :format=>'%Y-%m-%d %H:%M:%S')
+              activity_log = ActivityLog.new
+              activity_log.user_id = current_user.id
+              activity_log.controller = "Finance Log - POST CHECK"
+              activity_log.action = params[:order_id].to_s
+              activity_log.post_requests = params
+              activity_log.ip = request.remote_ip
+              activity_log.user_agent = request.user_agent
+              activity_log.created_at = now
+              activity_log.updated_at = now
+              activity_log.save
+            end
             if params[:create_transaction].present?
               
               validate_payment_types(params)

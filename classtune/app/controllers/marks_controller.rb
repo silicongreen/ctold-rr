5d37911b34315a8833d10c06efe276e2449cfe81
Fahim Mohammad Chowdhury <fahim.cse@gmail.com>
2020-12-22 17:28:05 +0600

    group report type 1

--- classtune/app/controllers/marks_controller.rb
+++ classtune/app/controllers/marks_controller.rb
@@ -672,6 +672,14 @@ class MarksController < ApplicationController
               data[k][3] = "<a href='/exam/comment_tabulation_pdf/#{exam_connect.id.to_s}#view=FitH' target='_blank'>Comment Entry</a>"
               data[k][4] = "<a href='/exam/generated_report5?connect_exam=#{exam_connect.id.to_s}&batch_id=#{exam_connect.batch_id.to_s}#view=FitH' target='_blank'>Results</a>"
               k = k+1
+            elsif school_id == 362
+              data[k] = []
+              data[k][0] = "<a href='/exam/continues/#{exam_connect.id.to_s}#view=FitH' target='_blank'>#{exam_connect_batch.to_s} (All Result)</a>"
+              data[k][1] = "#{exam_connect.name.to_s}"
+              data[k][2] = "<a href='/exam/marksheet/#{exam_connect.id.to_s}?subject_id=#{exam.subject_id.to_s}#view=FitH' target='_blank'>#{exam.subject_name.to_s} (Marksheet)</a>"
+              data[k][3] = "-"
+              data[k][4] = "<a href='/exam/generated_report5?connect_exam=#{exam_connect.id.to_s}&batch_id=#{exam_connect.batch_id.to_s}#view=FitH' target='_blank'>Results</a>"
+              k = k+1
             else
               data[k] = []
               data[k][0] = "<a href='/exam/continues/#{exam_connect.id.to_s}#view=FitH' target='_blank'>#{exam_connect_batch.to_s} (All Result)</a>"

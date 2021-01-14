f3014747e72301d5e7f69683d972b910d81acf3e
Fahim Mohammad Chowdhury <fahim.cse@gmail.com>
2021-01-14 16:34:54 +0600

    type 2 changes

--- classtune/app/controllers/attendances_controller.rb
+++ classtune/app/controllers/attendances_controller.rb
@@ -103,11 +103,9 @@ class AttendancesController < ApplicationController
     @date_today = @local_tzone_time.to_date
     if current_user.admin?
       @batches = Batch.active
-    elsif @current_user.privileges.map{|p| p.name}.include?('StudentAttendanceRegister')
-      @batches = Batch.active
     elsif @current_user.employee?
       @batches = @current_user.employee_record.batches
-      @batches += @current_user.employee_record.subjects.collect{|b| b.batch}  
+      @batches += @current_user.employee_record.subjects.collect{|b| b.batch}
       @batches = @batches.uniq unless @batches.empty
     end
     render :partial=>"subject_report"
@@ -581,11 +579,9 @@ class AttendancesController < ApplicationController
     @date_today = @local_tzone_time.to_date
     if current_user.admin?
       @batches = Batch.active
-    elsif @current_user.privileges.map{|p| p.name}.include?('StudentAttendanceRegister')
-      @batches = Batch.active
     elsif @current_user.employee?
       @batches = @current_user.employee_record.batches
-#      @batches += @current_user.employee_record.subjects.collect{|b| b.batch}
+      @batches += @current_user.employee_record.subjects.collect{|b| b.batch}
       @batches = @batches.uniq unless @batches.empty
     end
     render :partial=>"rollcall"

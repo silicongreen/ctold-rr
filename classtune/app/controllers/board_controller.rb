961dab516772a37d6e4d86898832b6e63a0c508e
Fahim <fahimcse@Fahims-MacBook-Air.local>
2021-03-17 12:21:08 +0600

    board sms number report and notice

--- classtune/app/controllers/board_controller.rb
+++ classtune/app/controllers/board_controller.rb
@@ -35,7 +35,7 @@ class BoardController < ApplicationController
     Spreadsheet.client_encoding = 'UTF-8'
     new_book = Spreadsheet::Workbook.new
     sheet1 = new_book.create_worksheet :name => 'student_list_exam'
-    row_first = ['SL','ID','Class Roll','Name','Father Name','Exam Reg. No.','Exam Roll No','Session','GPA']
+    row_first = ['SL','ID','Class Roll','Name','Father Name','Sms Number','Exam Reg. No.','Exam Roll No','Session','GPA']
     new_book.worksheet(0).insert_row(0, row_first)
     @board_exam = BoardExam.find(params[:id],:include=>["board_exam_name","board_exam_group","board_session"])
     @board_exam_students = BoardExamStudent.find_all_by_batch_id(params[:id2])
@@ -60,6 +60,7 @@ class BoardController < ApplicationController
       else
         tmp_row << ""
       end  
+      tmp_row << @std_info.sms_number
       tmp_row << @board_exam_student.registration
       tmp_row << @board_exam_student.roll
       tmp_row << @board_exam_student.session

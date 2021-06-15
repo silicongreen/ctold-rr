77e7a9796b0aabc1c5ce80036880ef9bdd3ba879
Fahim <fahim.cse@gmail.com>
2021-06-15 13:19:31 +0600

    assignment all download problem

--- classtune/app/controllers/exam_controller.rb
+++ classtune/app/controllers/exam_controller.rb
@@ -7095,7 +7095,7 @@ class ExamController < ApplicationController
           :footer => {:html => { :template=> 'layouts/pdf_empty_footer.html'}}
       end
     elsif MultiSchool.current_school.id == 319 or MultiSchool.current_school.id == 323 or MultiSchool.current_school.id == 325
-      if MultiSchool.current_school.id == 319  and (@connect_exam_obj.result_type == 2 or @connect_exam_obj.result_type == 4 or @connect_exam_obj.result_type == 3 or @connect_exam_obj.result_type == 5 or @connect_exam_obj.result_type == 7 or @connect_exam_obj.result_type == 8 or @connect_exam_obj.result_type == 9)
+      if MultiSchool.current_school.id == 319  and (@connect_exam_obj.result_type == 2 or @connect_exam_obj.result_type == 4 or @connect_exam_obj.result_type == 3 or @connect_exam_obj.result_type == 5 or @connect_exam_obj.result_type == 7 or @connect_exam_obj.result_type == 8 or @connect_exam_obj.result_type == 9 or @connect_exam_obj.result_type == 11 or @connect_exam_obj.result_type == 12)
         render :pdf => template,
           
           :save_only    => for_save,

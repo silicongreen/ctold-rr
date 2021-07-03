14c0ac263f169a57d05090d0001df877a3091f46
Fahim <fahim.cse@gmail.com>
2021-07-03 20:18:10 +0600

    note and other

--- classtune/app/controllers/exam_controller.rb
+++ classtune/app/controllers/exam_controller.rb
@@ -3115,7 +3115,7 @@ class ExamController < ApplicationController
       end
     end
     @class = params[:class]
-    if @connect_exam_obj.result_type.to_i == 13 or @connect_exam_obj.result_type.to_i == 14 or @connect_exam_obj.result_type.to_i == 15
+    if @connect_exam_obj.result_type.to_i == 13 or @connect_exam_obj.result_type.to_i == 14 or @connect_exam_obj.result_type.to_i == 15 or @connect_exam_obj.result_type.to_i == 17
       finding_data_sagc_covid()
     else
       finding_data5()

b1ddf024c6b59a6c770243737a629f66d49fc994
ahmabdullah <huffas@gmail.com>
2022-01-11 04:05:41 +0600

    Update exam_controller.rb

--- classtune/app/controllers/exam_controller.rb
+++ classtune/app/controllers/exam_controller.rb
@@ -3487,7 +3487,7 @@ class ExamController < ApplicationController
       end
     end
     @class = params[:class]
-    if @connect_exam_obj.result_type.to_i == 11 or @connect_exam_obj.result_type.to_i == 13 or @connect_exam_obj.result_type.to_i == 14 or @connect_exam_obj.result_type.to_i == 15 or @connect_exam_obj.result_type.to_i == 16
+    if @connect_exam_obj.result_type.to_i == 9 or @connect_exam_obj.result_type.to_i == 11 or @connect_exam_obj.result_type.to_i == 13 or @connect_exam_obj.result_type.to_i == 14 or @connect_exam_obj.result_type.to_i == 15 or @connect_exam_obj.result_type.to_i == 16
       finding_data_sagc_covid()
     elsif @connect_exam_obj.result_type.to_i == 17
       finding_data_sagc_25()

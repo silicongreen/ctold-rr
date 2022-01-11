4e0bd0f49730212027114aec00fc6529f146da9d
ahmabdullah <huffas@gmail.com>
2022-01-11 08:18:44 +0600

    Update exam_connect_merit_list.rb

--- classtune/app/models/exam_connect_merit_list.rb
+++ classtune/app/models/exam_connect_merit_list.rb
@@ -16,7 +16,7 @@
 #See the License for the specific language governing permissions and
 #limitations under the License.
 
-class Attendance < ActiveRecord::Base
+class ExamConnectMeritList < ActiveRecord::Base
   belongs_to :student
   belongs_to :batch
 

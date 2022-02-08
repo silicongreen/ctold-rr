25bf838b1bd1753e676facfc3e87056e303f65ee
ahmabdullah <huffas@gmail.com>
2022-02-08 06:30:06 +0600

    Update exam_controller.rb

--- classtune/app/controllers/exam_controller.rb
+++ classtune/app/controllers/exam_controller.rb
@@ -6903,7 +6903,8 @@ class ExamController < ApplicationController
        
     @exam_comment = ExamConnectComment.find_all_by_exam_connect_id(@connect_exam_obj.id)
     if (MultiSchool.current_school.id == 280 && @connect_exam_obj.result_type==2) or 
-        (MultiSchool.current_school.id == 323 && @connect_exam_obj.result_type==6)
+        (MultiSchool.current_school.id == 323 && @connect_exam_obj.result_type==6) or 
+        (MultiSchool.current_school.id == 362 && @connect_exam_obj.result_type==4)
       render :pdf => 'tabulation',
         :orientation => 'Landscape', :zoom => 1.00,
         :page_size => 'Legal',

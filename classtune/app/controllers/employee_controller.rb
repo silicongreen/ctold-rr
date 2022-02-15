d3bd99a94d7b9866d40908656c21842a05bdc989
ahmabdullah <huffas@gmail.com>
2022-02-15 09:38:43 +0600

    changes

--- classtune/app/controllers/employee_controller.rb
+++ classtune/app/controllers/employee_controller.rb
@@ -243,7 +243,7 @@ class EmployeeController < ApplicationController
 
         @assignment_register = Assignment.count(:conditions=>["date(created_at) = ? and employee_id = ?",@date_to_use.to_date,employee.id])
         @classwork_register = Classwork.count(:conditions=>["date(created_at) = ? and employee_id = ?",@date_to_use.to_date,employee.id])
-        @lesson_plan_register = Lessonplan.count(:conditions=>["author_id = ?",employee.user_id])
+        @lesson_plan_register = Lessonplan.count(:conditions=>["date(created_at) = ? and author_id = ?",@date_to_use.to_date,employee.user_id])
         temp << @entries
         temp << @assignment_register
         temp << @classwork_register

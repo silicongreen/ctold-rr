041957eacd07c60f4705265e2652dc8bc817ebd4
ahmabdullah <huffas@gmail.com>
2022-04-24 05:21:22 +0600

    changes

--- classtune/app/controllers/employee_controller.rb
+++ classtune/app/controllers/employee_controller.rb
@@ -62,7 +62,7 @@ class EmployeeController < ApplicationController
     @data = []
     center_align_format = Spreadsheet::Format.new :horizontal_align => :center,  :vertical_align => :middle,:left=>:thin,:right=>:thin,:top=>:thin,:bottom=>:thin
 
-    row_date = ['Report Date',date_to_use.strftime("%a %d %y")]
+    row_date = ['Report Date',@date_to_use.strftime("%a %d %y")]
     new_book.worksheet(0).insert_row(1, row_first)
 
     row_first = ['ID','Teacher Name','Department Name','Attendance','Total Class','Homework','Classwork','Lessonplan','Quiz','Class Routine']

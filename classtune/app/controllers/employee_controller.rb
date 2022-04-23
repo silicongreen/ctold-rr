1f07e9b79e8bfdd48247a7d885d41eddc3c1da6b
ahmabdullah <huffas@gmail.com>
2022-04-23 06:42:00 +0600

    changes

--- classtune/app/controllers/employee_controller.rb
+++ classtune/app/controllers/employee_controller.rb
@@ -127,7 +127,7 @@ class EmployeeController < ApplicationController
                 #    end
                 #  end
                   
-                end
+                # end
               else
                 @entries = @entries+1  
               end

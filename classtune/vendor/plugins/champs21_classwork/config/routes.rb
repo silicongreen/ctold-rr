ae0cca01eca4af79eac31cd61ee188a4173004c7
ahmabdullah <huffas@gmail.com>
2022-05-29 06:20:43 +0600

    changes

--- classtune/vendor/plugins/champs21_classwork/config/routes.rb
+++ classtune/vendor/plugins/champs21_classwork/config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   
  #classwork
-  map.resources :classworks,:collection=>{:get_classwork_filter=>[:get],:subject_classworks=>[:get]},:has_many => :classwork_answers
+  map.resources :classworks,:collection=>{:get_classwork_filter=>[:get],:subject_classworks=>[:get],:download_pdf=>[:get],:download_pdf_publush_date=>[:get],:download_excell=>[:get]},:has_many => :classwork_answers
  # map.resources :attendance_reports
 end

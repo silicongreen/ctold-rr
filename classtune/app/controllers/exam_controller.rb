b8ebe4c6a926a38eb136be45fea59c630259e404
ahmabdullah <huffas@gmail.com>
2022-06-13 01:47:42 +0600

    changes

--- classtune/app/controllers/exam_controller.rb
+++ classtune/app/controllers/exam_controller.rb
@@ -12221,7 +12221,7 @@ class ExamController < ApplicationController
                   term_mark_multiplier = 0.90
                 end
                 
-                if @connect_exam_obj.result_type == 5 or @connect_exam_obj.result_type == 6 or sub['grade_subject'].to_i == 1
+                if @connect_exam_obj.result_type == 5 or @connect_exam_obj.result_type == 11 or @connect_exam_obj.result_type == 6 or sub['grade_subject'].to_i == 1
                   term_mark_multiplier = 1.00
                 end
                 
@@ -12326,8 +12326,7 @@ class ExamController < ApplicationController
                     main_mark = (total_mark1_no_round.to_f+total_mark2_no_round.to_f)/(full_mark1.to_f+full_mark2.to_f)*100
                   else
                     main_mark = (total_mark1.to_f+total_mark2.to_f)/(full_mark1.to_f+full_mark2.to_f)*100
-                  end  
-                   
+                  end 
                   main_mark = main_mark.round()
                   mark_1_half = total_mark1_no_round.to_f/2.00
                   mark_2_half = total_mark2_no_round.to_f/2.00
@@ -12335,7 +12334,7 @@ class ExamController < ApplicationController
                   subject_mark = (total_mark1+total_mark2)/2
                   subject_mark = subject_mark.round()
                 elsif  exam_type == 2
-                  main_mark = total_mark2.to_f/full_mark2.to_f*100
+                  
                  
                   main_mark_no_round = total_mark2_no_round.to_f/full_mark2.to_f*100
                   subject_mark = total_mark2
@@ -12361,11 +12360,11 @@ class ExamController < ApplicationController
                   grand_total2_with_fraction = grand_total2_with_fraction+total_mark2_no_round
                   grand_total_with_fraction = grand_total_with_fraction+main_mark_no_round
                   
-                  if full_mark1 == 50 && main_mark1 == 44 && (@connect_exam_obj.result_type == 12 || @connect_exam_obj.result_type == 6 || @connect_exam_obj.result_type == 5  )
+                  if full_mark1 == 50 && main_mark1 == 44 && (@connect_exam_obj.result_type == 12 || @connect_exam_obj.result_type == 6 || @connect_exam_obj.result_type == 5 )
                     main_mark1 = 45
                     main_mark = 45
                   end
-                  if full_mark2 == 50 && main_mark2 == 44 && (@connect_exam_obj.result_type == 12 || @connect_exam_obj.result_type == 6 || @connect_exam_obj.result_type == 5  )
+                  if full_mark2 == 50 && main_mark2 == 44 && (@connect_exam_obj.result_type == 12 || @connect_exam_obj.result_type == 6 || @connect_exam_obj.result_type == 5 )
                     main_mark2 = 45
                   end
                 

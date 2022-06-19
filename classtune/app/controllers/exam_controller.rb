409d99f20f08a46dca22ecea4f3bbd9e409553c1
ahmabdullah <huffas@gmail.com>
2022-06-20 05:28:48 +0600

    changes

--- classtune/app/controllers/exam_controller.rb
+++ classtune/app/controllers/exam_controller.rb
@@ -13948,7 +13948,7 @@ class ExamController < ApplicationController
       @section_all_position_batch_final_term = {}
       last_grade = 0.0
       last_total = 0.0
-      abort(@student_list_first_term.inspect)
+      abort(@section_wise_position.inspect)
       unless @student_list.blank?
         position = 0
         @sorted_students = @student_list.sort

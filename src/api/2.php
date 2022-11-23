18423e896b3f8c43982f10dec7d26180ad9b605e
ahmabdullah <huffas@gmail.com>
2022-11-23 07:38:57 +0600

    changes

--- src/api/2.php
+++ src/api/2.php
@@ -14,5 +14,13 @@ if ($conn_source->connect_error)
     die("Connection failed: " . $conn_source->connect_error);
 }
 
+// Perform query
+if ($result = mysqli_query($conn_source, "SELECT * FROM `exam_connects` WHERE `name` LIKE '2nd Term Exam' AND `school_id` = 352 and published_date LIKE '2022%' ORDER BY `id` DESC ")) {
+    echo "Returned rows are: " . mysqli_num_rows($result);
+    // Free result set
+    mysqli_free_result($result);
+  }
+  
+  mysqli_close($con);
 echo 'jeje';
 exit;

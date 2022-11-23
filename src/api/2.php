b9c680f6bca778dc3bb5da8a9a7d068efdb8e097
ahmabdullah <huffas@gmail.com>
2022-11-23 07:37:21 +0600

    changes

--- src/api/2.php
+++ src/api/2.php
@@ -1,11 +1,9 @@
 <?php
-phpinfo();
-exit;
 ini_set('max_execution_time',0);
-$servername = "192.168.0.117";
-$username = "champs21";
-$password = "079366";
-$dbname_source = "company";
+$servername = "128.199.171.177";
+$username = "classtune";
+$password = "u[QXL=OF%D,F";
+$dbname_source = "classtune";
 
 // Create connection
 global $conn_source;
@@ -16,15 +14,5 @@ if ($conn_source->connect_error)
     die("Connection failed: " . $conn_source->connect_error);
 }
 
-include('simplehtmldom_1_5/simple_html_dom.php');
-for($i = 1;$i<31;$i++)
-{
-$html = file_get_html('http://www.crn.com/slide-shows/managed-services/300079672/2016-msp-500-elite-150.htm/pgno/0/'.$i);
-
-echo $company =  $html->find('div.slideCopy p',0)->plaintext; 
-echo $website =  $html->find('div.slideCopy p',3)->plaintext;
-//echo $link->href;
-echo $service =  str_replace("Services: ","",$html->find('div.slideCopy p',5)->plaintext);
-$sql = "insert into company_data (company,website,service) values ('".$company."','".$website."','".$service."')";
-$conn_source->query($sql);
-}
+echo 'jeje';
+exit;

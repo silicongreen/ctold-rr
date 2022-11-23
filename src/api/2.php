fd039be69d200080b1dbfd432c885a3e5ecf840d
ahmabdullah <huffas@gmail.com>
2022-11-23 07:41:00 +0600

    changes

--- src/api/2.php
+++ src/api/2.php
@@ -17,6 +17,13 @@ if ($conn_source->connect_error)
 // Perform query
 if ($result = mysqli_query($conn_source, "SELECT * FROM `exam_connects` WHERE `name` LIKE '2nd Term Exam' AND `school_id` = 352 and published_date LIKE '2022%' ORDER BY `id` DESC ")) {
     echo "Returned rows are: " . mysqli_num_rows($result);
+
+    while ($row = mysqli_fetch_row($result)) {
+        $connect_id = $row[0];
+        $batch_id = $row[2];
+        print("connect_id: ".$row[0]."\n");
+        print("Age: ".$row[1]."\n");
+     }
     // Free result set
     mysqli_free_result($result);
   }

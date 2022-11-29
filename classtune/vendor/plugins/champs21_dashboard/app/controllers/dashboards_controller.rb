062e0406fae50fb7fd486861c3a9e814b63b57d7
ahmabdullah <huffas@gmail.com>
2022-11-29 12:00:34 +0600

    changes

--- classtune/vendor/plugins/champs21_dashboard/app/controllers/dashboards_controller.rb
+++ classtune/vendor/plugins/champs21_dashboard/app/controllers/dashboards_controller.rb
@@ -4,6 +4,7 @@ class DashboardsController < ApplicationController
   filter_access_to :all
 
   def index
+      abort('hehe')
       unless MultiSchool.current_school.is_test_school == 2
         require "yaml"
         require "time"

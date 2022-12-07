6c25c90cdd290941507d52086d175cfc9c987410
ahmabdullah <huffas@gmail.com>
2022-12-07 10:10:21 +0600

    pnr

--- classtune/app/controllers/exam_controller.rb
+++ classtune/app/controllers/exam_controller.rb
@@ -6823,9 +6823,9 @@ class ExamController < ApplicationController
                 tmp_row << rt.to_s
               end
               if @connect_exam_obj.result_type.to_i == 3
-                tmp_row << rt.round().to_s + "  "  + value.id.to_s
+                tmp_row << rt.round().to_s
                 m_rt = rt
-                if key == "Dwing"
+                if value.id.to_s == "Dwing"
                   full_marks = 50
                   m_rt = (rt.to_f / full_marks) * 100
                 end

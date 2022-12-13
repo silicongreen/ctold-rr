67c6c037f40db299cf304b5fdcf4b032e780226a
ahmabdullah <huffas@gmail.com>
2022-12-13 11:09:47 +0600

    changes

--- classtune/app/controllers/exam_controller.rb
+++ classtune/app/controllers/exam_controller.rb
@@ -15015,16 +15015,17 @@ class ExamController < ApplicationController
                     divider = 4
                     class_test_mark = 0
                     class_test_full_mark = 0
-                    tab['exams'].each do |report|   
-                      if report['exam_category'] == '1'
-                        if !report['result'].blank? and !report['result'][report['exam_id']].blank? and !report['result'][report['exam_id']][subjects['id']].blank?  and !report['result'][report['exam_id']][subjects['id']]['marks_obtained'].blank?                                   
-                          class_test_mark = class_test_mark+report['result'][report['exam_id']][subjects['id']]['marks_obtained'].to_f
-                        end
-                        if !report['result'].blank? and !report['result'][report['exam_id']].blank? and !report['result'][report['exam_id']][subjects['id']].blank?  and !report['result'][report['exam_id']][subjects['id']]['full_mark'].blank?
-                            class_test_full_mark = class_test_full_mark+report['result'][report['exam_id']][subjects['id']]['full_mark'].to_f
+                      tab['exams'].each do |report|   
+                        abort(report.inspect)
+                        if report['exam_category'] == '1'
+                          if !report['result'].blank? and !report['result'][report['exam_id']].blank? and !report['result'][report['exam_id']][sub['id']].blank?  and !report['result'][report['exam_id']][sub['id']]['marks_obtained'].blank?                                   
+                            class_test_mark = class_test_mark+report['result'][report['exam_id']][sub['id']]['marks_obtained'].to_f
                           end
+                          if !report['result'].blank? and !report['result'][report['exam_id']].blank? and !report['result'][report['exam_id']][sub['id']].blank?  and !report['result'][report['exam_id']][sub['id']]['full_mark'].blank?
+                              class_test_full_mark = class_test_full_mark+report['result'][sub['exam_id']][subjects['id']]['full_mark'].to_f
+                            end
+                        end
                       end
-                    end
                     if class_test_full_mark.to_i == 30 && full_mark_all == 50
                         divider = 6
                     end

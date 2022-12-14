b09e44fdd424c17c2b4b7c23edbb19a0803cc53d
ahmabdullah <huffas@gmail.com>
2022-12-14 08:37:32 +0600

    changes

--- classtune/app/controllers/exam_controller.rb
+++ classtune/app/controllers/exam_controller.rb
@@ -16058,13 +16058,13 @@ class ExamController < ApplicationController
                       grand_total = grand_total+subject_full_marks
                       grand_total1_with_fraction = grand_total1_with_fraction+total_mark1_no_round
                       
-                      //grand_total1_with_fraction_marks << total_mark1_no_round
+                      #grand_total1_with_fraction_marks << total_mark1_no_round
                       
                       grand_total2_with_fraction = grand_total2_with_fraction+main_mark_no_round
                       
                       grand_total_with_fraction = grand_total_with_fraction+main_mark_no_round
                       
-                      //grand_total_with_fraction_marks << main_mark_no_round
+                      #grand_total_with_fraction_marks << main_mark_no_round
                       
                       if fourth_subject.blank?
                         grade = GradingLevel.percentage_to_grade(main_mark1, @batch.id)

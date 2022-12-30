3d409daf063046f9d1a78c9b3276a3846bcf49fc
ahmabdullah <huffas@gmail.com>
2022-12-31 01:12:15 +0600

    changes

--- classtune/app/controllers/attendances_controller.rb
+++ classtune/app/controllers/attendances_controller.rb
@@ -313,7 +313,7 @@ class AttendancesController < ApplicationController
             @subject_att_register = SubjectAttendanceRegister.all(:select=>"count(id) as total_register,subject_id",:conditions=>["batch_id in (" + batch_ids.join(",") + ") and attendance_date >= ? and attendance_date <= ?",@date_form,@date_to],:group=>"subject_id")
             
             @subject_att = SubjectAttendance.all(:select=>"count(id) as total_absent,student_id",:conditions=>["batch_id in (" + batch_ids.join(",") + ") and attendance_date >= ? and attendance_date <= ? and is_late = 0",@date_form,@date_to],:group=>"student_id")
-            abort(@subject_att_register.inspect)
+
           end
         elsif @current_user.student?
         elsif @current_user.student?
